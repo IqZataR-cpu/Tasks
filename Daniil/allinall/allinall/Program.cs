8515558298068034381f0a17551efdb367978055
DESKTOP-3OGPFAT\danii <daniil.dg.galkin@gmail.com>
2021-02-10 21:51:30 +0300

    add allinall  with EZ tasks Galkin

---
+++ Daniil/allinall/allinall/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace allinall
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string userСhoice;
+            Console.WriteLine("Добро пожаловать в программу, для выбора уровня сложности введите: Легки, Средний, Тяжелый, для выхода напиши ВЫХОД");
+            
+
+            while (true)
+            {
+                userСhoice = Console.ReadLine();
+
+                if (userСhoice == "Легкий")
+                {
+                    easyDifficultyLevel.taskSelectionEZ();//вызов класса
+                }
+                else if (userСhoice == "Тяжелый")
+                {
+                    Console.WriteLine("Введите коректные данные22");//вызов класса
+                }
+                else if (userСhoice == "Средний")
+                {
+                    Console.WriteLine("Введите коректные данные333");//вызов класса
+                }
+                else if (userСhoice == "ВЫХОД")
+                {
+                    Console.WriteLine("Спасибо за использование програамы ");//вызов класса
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введите коректные данные");
+                }
+
+
+            }
+
+
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ Daniil/allinall/allinall/easyDifficultyLevel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allinall
+{
+    class easyDifficultyLevel
+    {
+        public static void taskSelectionEZ()
+        {
+            string userСhoice;
+            Console.WriteLine("Вы выбрали Легкую сложность для выбора задачи напишите: Условия, Циклы, Массивы, Функции, для выхода напиши ВЫХОД");
+            while (true)
+            {
+                userСhoice = Console.ReadLine();
+
+                if (userСhoice == "Условия")
+                {
+                    Console.WriteLine("Из двух случайных чисел, одно из которых четное, а другое нечетное, определить и вывести на экран нечетное число.");//вызов класса
+                    determinesParityOfNum();
+                }
+                else if (userСhoice == "Циклы")
+                {
+                    Console.WriteLine("Вывести на экран ряд чисел Фибоначчи, состоящий из n элементов." +
+                                            "Числа Фибоначчи – это элементы числовой последовательности" +
+                                            "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …, в которой каждое последующее число равно сумме двух предыдущих.");//вызов класса
+                    listOfFibonacci();
+                }
+                else if (userСhoice == "Массивы")
+                {
+                    Console.WriteLine("Даны целочисленные массивы X и Y с разным количеством элементов. " +
+                        "Найти минимальные элементы этих массивах среди элементов, имеющих четный индекс.");//вызов класса
+
+                }
+                else if (userСhoice == "Функции")
+                {
+                    Console.WriteLine("Вы выбрали задачу на ФУНКЦИИ.");//вызов класса
+                    Console.WriteLine("Введите 2 числа.");//вызов класса
+                    int result = 0;
+                    int firstNum = int.Parse(Console.ReadLine());
+                    int secondNum = int.Parse(Console.ReadLine());
+                    multiplicationWithRecursion(firstNum, secondNum, result);
+                    Console.WriteLine("Вы выбрали Легкую сложность для выбора задачи напишите: Условия, Циклы, Массивы, Функции, для выхода напиши ВЫХОД");
+                }
+                else if (userСhoice == "ВЫХОД")
+                {
+                    Console.WriteLine("Вы будете перенаправлены в предыдущие меню ");//вызов класса
+                    Console.WriteLine("Добро пожаловать в программу, для выбора уровня сложности введите: Легки, Средний, Тяжелый, для выхода напиши ВЫХОД");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введите коректные данные");
+                }
+            }
+        }
+
+
+
+
+
+
+        public static void determinesParityOfNum() 
+        {
+            Console.WriteLine("Вы выбрали задачу на УСЛОВИЯ.");
+            Console.WriteLine("Введите два числа");
+            int firstNum = int.Parse(Console.ReadLine());           
+            if (firstNum % 2 != 0)
+            {
+                Console.WriteLine($"число {firstNum.ToString()} - нечетное");
+                Console.WriteLine("Вы выбрали Легкую сложность для выбора задачи напишите: Условия, Циклы, Массивы, Функции, для выхода напиши ВЫХОД");
+            }
+            else 
+            {
+                Console.WriteLine($"число {firstNum.ToString()} - четное");
+                Console.WriteLine("Вы выбрали Легкую сложность для выбора задачи напишите: Условия, Циклы, Массивы, Функции, для выхода напиши ВЫХОД");
+            }
+        }
+
+
+        public static void listOfFibonacci()
+        {
+            Console.WriteLine("Вы выбрали задачу на ЦИКЛЫ.");
+            int fibonachiNum = int.Parse(Console.ReadLine());
+            int fibonachiFirst = 0;            
+            int fibonachiSecond = 1;
+            int fibonachiRes;
+            
+            for (int i = 0; i < fibonachiNum; i++)
+            {
+                fibonachiRes = fibonachiFirst + fibonachiSecond;
+                fibonachiFirst = fibonachiSecond;
+                fibonachiSecond = fibonachiRes;
+                Console.WriteLine($"{fibonachiRes}, ");
+            }
+            Console.WriteLine("Вы выбрали Легкую сложность для выбора задачи напишите: Условия, Циклы, Массивы, Функции, для выхода напиши ВЫХОД");
+
+
+        }
+
+
+        public static void multiplicationWithRecursion(int firstNum, int secondNum, int result)
+        {
+            result = result + firstNum;
+            secondNum--;
+            if (secondNum != 0)
+            {
+                multiplicationWithRecursion(firstNum, secondNum, result);
+            }
+            else { Console.WriteLine($"{result}"); }
+        }
+    }
+}
