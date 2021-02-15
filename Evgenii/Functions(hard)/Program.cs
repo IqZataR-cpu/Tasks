2e855e43a47c36edc0d7d9b02fe9505a96d823bb
Kerollinenot <zhenya.petrovskiy.2015@mail.ru>
2021-02-15 22:14:33 +0300

    Create program Functions(hard)

---
+++ Evgenii/Functions(hard)/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Functions_hard_
+{
+    class Program
+    {
+        private static int[] array = new int[] { 1, 3, 6, 8, 11, 25, 57, 65, 100 };
+
+        static void Main(string[] args)
+        {
+            int enteredValue;
+            int? index;
+
+            Console.WriteLine("Введите значение массива");
+            enteredValue = int.Parse(Console.ReadLine());
+
+            index = searchIndex(enteredValue);
+
+            if (index == null)
+            {
+                Console.WriteLine("Значения не существует");
+            }
+            else
+            {
+                Console.WriteLine(index + 1);
+            }
+        }
+
+        private static int? searchIndex(int? value)
+        {
+            int middleOfArray = array.Length / 2;
+            int counter = 1;
+            int? index;
+            index = null;
+
+            Console.WriteLine(array.Length);
+            while ((middleOfArray >= 0 && counter < array.Length) ||
+                   (middleOfArray <= array.Length && counter < array.Length))
+            {
+                counter = counter * 2;
+
+                if (array[middleOfArray] == value)
+                {
+                    index = middleOfArray;
+                    break;
+                }
+                if (array[middleOfArray] > value)
+                {
+                    middleOfArray = middleOfArray - array.Length / counter;
+                }
+                if (array[middleOfArray] < value)
+                {
+                    middleOfArray = middleOfArray + array.Length / counter;
+                }
+            }
+
+            return index;
+        }
+    }
+}
