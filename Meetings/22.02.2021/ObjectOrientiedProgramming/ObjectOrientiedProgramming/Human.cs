39c10089fa861272214e08fd52ed39114364d1e7
IqZataR <Mikcha.y.12@gmail.com>
2021-02-22 21:15:53 +0300

    Meeting 22.02.2021

---
+++ Meetings/22.02.2021/ObjectOrientiedProgramming/ObjectOrientiedProgramming/Human.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ObjectOrientiedProgramming
+{
+    public abstract class Human
+    {
+        private string _name;
+        private float _health;
+        private int _speed;
+        private int _attackDamage;
+        private int _armor;
+        private int _position = 0;
+
+        public int Position
+        {
+            get => _position;
+        }
+        
+        protected Human(string name, float health, int speed, int attackDamage, int armor)
+        {
+            _name = name;
+            _health = health;
+            _speed = speed;
+            _attackDamage = attackDamage;
+            _armor = armor;
+        }
+
+        public abstract void Idle();
+
+        public abstract void Attack(Human target);
+
+        // хп не должно уйти в отрицательное значение
+        public abstract void TryApplyDamage(int damage);
+
+        public void MoveForward()
+        {
+            _position += _speed;
+        }
+
+        public void MoveBack()
+        {
+            _position -= _speed;
+        }
+        
+        public virtual void Print()
+        {
+            Console.WriteLine($"Имя: {_name}");
+            Console.WriteLine($"Здоровье: {_health}");
+            Console.WriteLine($"Скорость:{_speed}");
+            Console.WriteLine($"Наносимый урон:{_attackDamage}");
+            Console.WriteLine($"Броня:{_armor}");
+            Console.WriteLine($"Текущая позиция:{_position}");
+        }
+    }
+}
---
+++ Meetings/22.02.2021/ObjectOrientiedProgramming/ObjectOrientiedProgramming/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ObjectOrientiedProgramming
+{
+    //
+    // Создать базовый класс Human
+    // Общие поля: здоровье, скорость,
+    // атака(количество наносимого урона),
+    // броня(когда получает урон, Здоровье = Здоровье - (атака - 10% от брони)),
+    // местоположение(линейное движение от 0 и далее)
+    //
+    // Базовые методы: Отдых, Атака, Движение, Получение урона
+    //
+    // Создать подклассы: Warrior, Archer.
+    // Реализовать базовые методы для подтипов.
+    //
+    // Создать небольшое взаимодействие между несколькими объектами,
+    // без консольного выбора 
+    //
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human warrior = new Warrior("Артем",100, 5, 10, 1);
+            Human archer = new Archer("Миша", 100, 5, 10, 1);
+
+            warrior.Print();
+            archer.Print();
+            
+            warrior.Attack(archer);
+            archer.Attack(warrior);
+            
+            warrior.Print();
+            archer.Print();
+            
+            warrior.MoveBack();
+            archer.MoveForward();
+            warrior.Idle();
+            archer.Idle();
+            
+            warrior.Print();
+            archer.Print();
+        }
+    }
+}
