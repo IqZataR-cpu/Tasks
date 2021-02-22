b65b25d009504b02f6d450ba44f15b1f0d8d1b07
yo <yo>
2021-02-22 23:54:49 +0300

    Complete easy task

---
+++ Timofei/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  n             = 15;
+            int  FistNumber    = 0;
+            int  SecondNumber  = 1;
+            int  NeutralNumber = 1;
+            
+            Console.Write(FistNumber);
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($" " + NeutralNumber);
+                
+                NeutralNumber = FistNumber + SecondNumber;
+                
+                FistNumber    = SecondNumber;
+                
+                SecondNumber  = NeutralNumber;
+            }
+        }
+    }
+}
