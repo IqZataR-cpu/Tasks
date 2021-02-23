12c0b6ddefdfb920cdfb1d7677b2e9471a8e2a4a
IqZataR <Mikcha.y.12@gmail.com>
2021-02-23 17:45:48 +0300

    Fixme for Timofei part one

--- Timofei/ConsoleApp3/ConsoleApp3/Program.cs
+++ Timofei/ConsoleApp3/ConsoleApp3/Program.cs
@@ -2,25 +2,35 @@
 
 namespace ConsoleApp3
 {
+    // В целом хорошо, но есть фичи, которые тебе неизвестны,
+    // соответственно прочитал, обдумал, запомнил, стал успешным
     class Program
     {
         static void Main(string[] args)
         {
+            // Прикольное объявление, мне нравится,
+            // все выведено на один уровень табуляции.
+            // Не корректное название переменной "n" - стоило назвать FibbonachiLength или АНАЛогами.
+            // Здесь душильня так что грамматические ошибки тоже учитываются FistNumber != FiRstNumber
             int  n             = 15;
             int  FistNumber    = 0;
             int  SecondNumber  = 1;
+            // Более подходящее название Temp, субъективно.
             int  NeutralNumber = 1;
             
             Console.Write(FistNumber);
             
+            // не нужно ставить Enter после каждого присваивания,
+            // разделяй на логические блоки, в данном случае их 2 
+            // Первый - Вывод на консоль
+            // Второй - Блок инициализации переменных(блок присваивания значений)
+            // Пример ниже
             for (int i = 0; i < n; i++)
             {
                 Console.Write($" " + NeutralNumber);
                 
                 NeutralNumber = FistNumber + SecondNumber;
-                
                 FistNumber    = SecondNumber;
-                
                 SecondNumber  = NeutralNumber;
             }
         }
