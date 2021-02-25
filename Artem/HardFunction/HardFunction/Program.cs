a7dcf9de05fe8c79ab2d28f9cb2689fbea4ac60d
Mikuzen <Mikuzen@github.com>
2021-02-25 11:50:05 +0300

    Fixed hard Function

--- Artem/HardFunction/HardFunction/Program.cs
+++ Artem/HardFunction/HardFunction/Program.cs
@@ -2,25 +2,24 @@
 
 namespace HardFunction
 {
-    // В целом все прекрасно, уже приятно смотреть,
-    // но не смог реализовать правильный алгоритм,
-    // поэтому придется доработать саму функцию поиска
     class Program
     {
         static void Main(string[] args)
         {
-            // Для большей читаемости дели такие объявления на 2 строки, пример ниже
+
             int[] numbers = new int[16] {
                 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
-                2048, 4096, 8192, 16384, 32768, 65536 
+                2048, 4096, 8192, 16384, 32768, 65536
             };
             int inputNumber;
             int numberOfElementArray;
-
+            int lengthArray = numbers.Length - 1;
+            int rigthBorderArray = lengthArray;
+            int leftBorderArray = 0;
             Console.WriteLine("Введите число");
             inputNumber = int.Parse(Console.ReadLine());
 
-            numberOfElementArray = BinarySearching(numbers, inputNumber);
+            numberOfElementArray = BinarySearching(numbers, inputNumber, leftBorderArray, rigthBorderArray) + 1;
 
             if (numberOfElementArray == 0)
             {
@@ -28,77 +27,57 @@ namespace HardFunction
                 return;
             }
 
-            // Также длинные строковые выражения стараемся сделать с переносом на следующую строку, пример ниже
             Console.WriteLine($"Число {inputNumber} существует в массиве " +
                               $"и оно находится на {numberOfElementArray} месте");
         }
 
-        
-        // Не до конца понял принцип бинарного поиска,
-        // ты делишь массив на 2 и потом отдельно по левой и правой части проходишься полностью
-        // Это не бинарный поиск.
-        // Бинарный поиск подразумевает под собой постоянное деление на 2 части массива
-        // и сравнение среднего элемента с тем который нужно найти
-        // То есть ты снова должен найти середину массива в левой части,
-        // если число меньше середины и в правой, если больше и так до тех пор пока не найдешь число.
-        // Поэтому функция так хорошо работает с рекурсией, но не обязательно ее делать именно такой(рекурсивной).
-        static int BinarySearching(int[] numbers, int number)
+        static int BinarySearching(int[] numbers, int number, int leftBorderArray, int rightBorderArray)
         {
-            // zero не корректное название, тут нужно было назвать так NUMBER_NOT_FOUND
-            // для констант применяет ALL_CAPS стиль написания.
-            const int zero = 0;
-            int lengthArray = numbers.Length - 1;
-            int middleArray = lengthArray / 2;
 
-            // Это круто, но у программистов не принято так писать,
-            // такое объявление заменяется на функции с аналогичным именем.
-            // На будущее, можно было сделать класс, который принимает на вход массив
-            // И создать метод поиска в массиве,
-            // тогда пришлось бы в методе указывать на один параметр меньше
-            // Также создать дополнительно приватные методы,
-            // названия которых соответствуют твоим булевым выражениям
-            // это гораздо более читабельно и не вызовет вопросов о твоем образовании
-            // Я знаю, что я этому научил, но я этому научил для того,
-            // чтобы в последствии это можно было перенести на функции, как в примере выше.
-            // Также не совсем корректное название,
-            // можно описать цель сравнения двумя словами isFind или isExist,
-            // поскольку мы работаем с контекстом number, видим это по параметру
-            // и метод называется Searching, то будет понятно что именно isExist/isFind
-            bool isNumberEqualsNumberOfArray = number == numbers[middleArray];
-            if (isNumberEqualsNumberOfArray)
+            const int NUMBERNOTFOUND = 0;
+            int middleArray = (leftBorderArray + rightBorderArray);
+            // Используется для поиска крайнего правого значение для передачи его правильного места в массиве
+            if (middleArray % 2 == 1)
+            {
+                middleArray /= 2;
+                middleArray += 1;
+            }
+
+            bool isFind = number == numbers[middleArray];
+            bool isNotFind = (leftBorderArray - middleArray == 0 && rightBorderArray - middleArray == 1);
+
+            if (isFind)
             {
                 return middleArray;
             }
 
-            // Здесь все логично, но заменяемо методом,
-            // но только с условием что сделано будет через класс
-            bool isNumberMoreThanMiddle = number > numbers[middleArray];
-            if (isNumberMoreThanMiddle)
+            if (isNotFind)
             {
-                for (int i = middleArray + 1; i <= numbers.Length; i++)
-                {
-                    isNumberEqualsNumberOfArray = number == numbers[i];
+                return NUMBERNOTFOUND;
+            }
 
-                    if (isNumberEqualsNumberOfArray)
-                    {
-                        return i + 1;
-                    }
+            bool isNumberMoreThanMiddle = number > numbers[middleArray];
+
+            if (isNumberMoreThanMiddle)
+            {       
+                
+                if (!isFind)
+                {
+                    return BinarySearching(numbers, number, middleArray, rightBorderArray);
                 }
+
             }
             else
             {
-                for (int i = middleArray - 1; i >= 0; i--)
-                {
-                    isNumberEqualsNumberOfArray = number == numbers[i];
 
-                    if (isNumberEqualsNumberOfArray)
-                    {
-                        return i + 1;
-                    }
+                if (!isFind)
+                {
+                    return BinarySearching(numbers, number, leftBorderArray, middleArray);
                 }
+
             }
 
-            return zero;
+            return NUMBERNOTFOUND;
         }
     }
 }
