7eb540ad27dc167f486e192c4c0e980343244bbd
yo <yo>
2021-03-06 18:03:56 +0300

    Complete ease array task

---
+++ Timofei/ConsoleApp8/ConsoleApp8/Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    public class Functions
+    {
+        public static int[] FillArray(int[] array)
+        {
+           
+            Random rnd = new Random();
+            // Вносим данные в массив.
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10,10);
+                Console.Write($" {array[i]}");
+            }
+
+            Console.WriteLine();
+            return array;
+        }
+
+        public static int FindMinimum(int[] array)
+        {
+            int min = array[0];
+            // Проверяем четность\нечетность. Выдялем мин элемент.
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (min > array[i])
+                    {
+                        min = array[i];
+                    }
+                }
+            }
+            
+            return min;
+        }
+    }
+}
---
+++ Timofei/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        private int _campot;
+        public int Campot => _campot;
+        
+        public static void Main(string[] args)
+        { 
+            Random rnd = new Random();
+
+            int[] arrayOne = new int[rnd.Next(1,10)]; 
+            int[] arrayTwo = new int[rnd.Next(1,10)];
+            
+            arrayOne = Functions.FillArray(arrayOne);
+            arrayTwo = Functions.FillArray(arrayTwo);
+            
+            Console.WriteLine($"Минимальный четный элемент первого массива равен - {Functions.FindMinimum(arrayOne)}");
+            Console.WriteLine($"Минимальный четный элемент второго массива равен - {Functions.FindMinimum(arrayTwo)}");
+        }
+    }
+}
