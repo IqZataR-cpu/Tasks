64519ef9ca8974b86402a86101f7189d00559237
yo <yo>
2021-03-09 00:12:04 +0300

    Complete middle task Cycles

---
+++ Timofei/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Задайте число");
+            string setNumber = Console.ReadLine();
+            
+            Console.WriteLine($"Задайте цифру которую необходимо удалить");
+            string deleteNumber = Console.ReadLine();
+            
+            Console.WriteLine($"____________________");
+            for (int i = 0; i < setNumber.Length; i++)
+            {
+                if (Convert.ToChar(deleteNumber) != Convert.ToChar(setNumber[i]))
+                {
+                    Console.Write(setNumber[i]);
+                }
+            }
+        }
+    }
+}
