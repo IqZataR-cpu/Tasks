5aa1b4c75c6d3496f00b8bcb5376cffa169cb8ac
yo <yo>
2021-03-16 00:04:43 +0300

    Completed Hard task by Cycles.

---
+++ Timofei/ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение.");
+            int  naturalNumber = Convert.ToInt32(Console.ReadLine());
+            
+            // Пока введенное число не будет равно 1 (единице), выполняем предписанные условия.
+            while (naturalNumber != 1)
+            {
+                // Четное число всегда будет равно 1.
+                if (naturalNumber % 2 == 0) naturalNumber = naturalNumber / 2;
+                else
+                {
+                    // Необходимо преобразовать нечетное число в четное. 
+                    while (naturalNumber % 2 != 0)
+                    {
+                        naturalNumber = (naturalNumber * 3 + 1) / 2;
+                    } 
+                }
+            }
+            Console.WriteLine(naturalNumber);
+        }
+    }
+}
