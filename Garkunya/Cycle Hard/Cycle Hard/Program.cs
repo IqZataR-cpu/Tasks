933826e03c0f0b9fc5dfcbb8425bd0cb270a1161
Kerollinenot <zhenya.petrovskiy.2015@mail.ru>
2021-03-15 23:10:08 +0300

    fixMe Cycle_Hard

--- Garkunya/Cycle Hard/Cycle Hard/Program.cs
+++ Garkunya/Cycle Hard/Cycle Hard/Program.cs
@@ -9,35 +9,35 @@ namespace Cycle_Hard
             Console.Write("Введите любое натуральное число: ");
             
             int divisibleNumber = int.Parse(Console.ReadLine());
-            int ready = 0;
+            int ready = 0; //Поменяй на bool значение
             int countOperation = 0;
 
             if (divisibleNumber == 0)
             {
                 Console.WriteLine("Нельзя преобразовать 0!");
             }
-            else
+            else 
             {
-               while (ready == 0)
-               {
-                   if (divisibleNumber == 2)
-                   {
-                       ready++;
-                       countOperation++;
-                       Console.WriteLine("Получилось!");
-                       Console.WriteLine("Количество операций: {0}", countOperation);
-                   }
-                   else if (divisibleNumber % 2 == 0)
-                   {
-                       divisibleNumber = divisibleNumber / 2;
-                       countOperation++;
-                   }
-                   else
-                   {
-                       divisibleNumber = (divisibleNumber * 3 + 1) / 2;
-                       countOperation++;
-                   }
-               }
+                while (ready == 0)
+                {
+                    if (divisibleNumber == 2) //Переделай архитектуру. 
+                    {
+                        ready++;
+                        countOperation++;
+                        Console.WriteLine("Получилось!");
+                        Console.WriteLine($"Количество операций: {0}", countOperation); // Console.WriteLine($"Ляляля {counter} ляляля");
+                    }
+                    else if (divisibleNumber % 2 == 0)
+                    {
+                        divisibleNumber = divisibleNumber / 2;
+                        countOperation++;
+                    }
+                    else
+                    {
+                        divisibleNumber = (divisibleNumber * 3 + 1) / 2;
+                        countOperation++;
+                    }
+                }
             }
         } 
     }
