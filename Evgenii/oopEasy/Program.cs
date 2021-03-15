00b1929981e0c1cdc49cc58c40af0e28ba2b3a20
Kerollinenot <zhenya.petrovskiy.2015@mail.ru>
2021-03-15 23:10:42 +0300

    correct programms

--- Evgenii/oopEasy/Program.cs
+++ Evgenii/oopEasy/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-
-
-namespace oopEasy
+﻿namespace oopEasy
 {
     class Program
     {
@@ -40,42 +37,5 @@ namespace oopEasy
         public void Attack(IDamagable target);
 
         public void TakeDamage(int damage);
-    }
-
-    public abstract class Orc : IDamagable
-    {
-        protected string _name;
-        protected int _health;
-        protected int _stamina;
-        protected int _armor;
-        protected int _damage;
-        protected int _moveSpeed;
-        protected int _position = 0;
-
-        protected Orc(string name)
-        {
-            _name = name;
-        }
-
-        public virtual void Attack(IDamagable target)
-        {
-
-        }
-
-        public virtual void TakeDamage(int damage)
-        {
-
-        }
-
-        public virtual void Print()
-        {
-            Console.WriteLine($"\nИмя: {_name}");
-            Console.WriteLine($"Здоровье: {_health}");
-            Console.WriteLine($"Скорость:{_stamina}");
-            Console.WriteLine($"Наносимый урон:{_armor}");
-            Console.WriteLine($"Броня:{_damage}");
-            Console.WriteLine($"Скорость:{_moveSpeed}");
-            Console.WriteLine($"Текущая позиция:{_position}\n");
-        }
     }
 }
