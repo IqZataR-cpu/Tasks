74e9489701abdc7d623a694aee0aebd521731a2f
Mikuzen <Mikuzen@github.com>
2021-03-16 00:27:07 +0300

    Fix hard loop

--- Timofei/ConsoleApp19/ConsoleApp19/Program.cs
+++ Timofei/ConsoleApp19/ConsoleApp19/Program.cs
@@ -7,11 +7,16 @@ namespace ConsoleApp19
         static void Main(string[] args)
         {
             Console.WriteLine("Введите значение.");
-            int  naturalNumber = Convert.ToInt32(Console.ReadLine());
+            int naturalNumber = Convert.ToInt32(Console.ReadLine());
             
+            // Не забываем про пустые строки после блоков цикла
+
             // Пока введенное число не будет равно 1 (единице), выполняем предписанные условия.
             while (naturalNumber != 1)
             {
+                // Условие одной строкой лучше записывать тогда, когда
+                // Не следует else после этого действия
+
                 // Четное число всегда будет равно 1.
                 if (naturalNumber % 2 == 0) naturalNumber = naturalNumber / 2;
                 else
