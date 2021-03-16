cc8d9e9dbc3a31d69a49c3d7bc6482f411c2ba2a
yo <yo>
2021-03-16 20:31:54 +0300

    Complete hard task by Array

--- Timofei/ConsoleApp19/ConsoleApp19/Program.cs
+++ Timofei/ConsoleApp19/ConsoleApp19/Program.cs
@@ -8,9 +8,7 @@ namespace ConsoleApp19
         {
             Console.WriteLine("Введите значение.");
             int naturalNumber = Convert.ToInt32(Console.ReadLine());
-            
-            // Не забываем про пустые строки после блоков цикла
-
+        
             // Пока введенное число не будет равно 1 (единице), выполняем предписанные условия.
             while (naturalNumber != 1)
             {
