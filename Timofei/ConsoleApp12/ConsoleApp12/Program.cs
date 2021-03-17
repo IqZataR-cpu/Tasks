61f5086b081f352d4518cbd371643f2f31adea87
Kerollinenot <zhenya.petrovskiy.2015@mail.ru>
2021-03-17 22:11:10 +0300

    FixMe for Timofey (Usloviya medium)

--- Timofei/ConsoleApp12/ConsoleApp12/Program.cs
+++ Timofei/ConsoleApp12/ConsoleApp12/Program.cs
@@ -6,9 +6,9 @@ namespace ConsoleApp12
     {
         static void Main(string[] args)
         {
-            int a = Int32.Parse(Console.ReadLine());
-            int b = Int32.Parse(Console.ReadLine());
-            int c = Int32.Parse(Console.ReadLine());
+            int a = Int32.Parse(Console.ReadLine()); //Называй правильно переменные. Непонятно, что за a, b, c.Переменные должны называться так,
+            int b = Int32.Parse(Console.ReadLine()); //чтобы было понятно, для чего они предназначены. Допустим, вместо переменной "a" ты мог бы её назвать
+            int c = Int32.Parse(Console.ReadLine()); //"firstInputValue или, на крайний случай, если условием задан именно параметр a, то inputValueA
             double d;
             double x1,x2;
             
@@ -25,7 +25,7 @@ namespace ConsoleApp12
                     x1 = -b / 2 * a;
                     Console.WriteLine($"x1, x2 = {x1}");
                 } 
-                else if (d > 0)
+                else if (d > 0) //лишнее условие. У тебя и так проверилось меньше 0 и равно 0. Осталось только > 0 и это можно просто добавить как else, а не else if
                 {
                     x1 = (-b + Math.Sqrt(d)) / (2 * a);
                     x2 = (-b - Math.Sqrt(d)) / (2 * a);
