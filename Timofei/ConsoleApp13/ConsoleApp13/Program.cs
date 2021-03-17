8f6b58d853e4ebcd4ce0cb5adc07c7b785f133db
yo <yo>
2021-03-17 16:59:16 +0300

    Fix middle task by Cycles

--- Timofei/ConsoleApp13/ConsoleApp13/Program.cs
+++ Timofei/ConsoleApp13/ConsoleApp13/Program.cs
@@ -6,28 +6,19 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-            // Знак $ используется, когда ты хочешь вывести
-            // Значение какой-то переменной помимо текста
-            Console.WriteLine($"Задайте число");
+            Console.WriteLine("Задайте число");
             string setNumber = Console.ReadLine();
             
             Console.WriteLine($"Задайте цифру которую необходимо удалить");
             string deleteNumber = Console.ReadLine();
-
-            // Зачем вообще эта строка снизу???
-            // Если ты хотел разделить текста друг от друга
-            // То используй cтроковый литерал \n
-            Console.WriteLine($"____________________");
-            // Не забываем разделять логические блоки 
-            // В программе пустыми строками
+            
+            Console.WriteLine();
+            
             for (int i = 0; i < setNumber.Length; i++)
             {
-                // Чтобы было понятней, что конкретно проверяется
-                // В условии используй переменную типо bool
-                // И припиши значение условия ей
-                // Например, bool isNotExistNumberInString =
-                // = Convert.ToChar(deleteNumber) != Convert.ToChar(setNumber[i])
-                if (Convert.ToChar(deleteNumber) != Convert.ToChar(setNumber[i]))
+                bool isNotExistNumberInString = Convert.ToChar(deleteNumber) != Convert.ToChar(setNumber[i]);
+               
+             if (isNotExistNumberInString)
                 {
                     Console.Write(setNumber[i]);
                 }
