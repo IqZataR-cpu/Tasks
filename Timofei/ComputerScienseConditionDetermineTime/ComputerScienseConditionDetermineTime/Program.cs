9b067b352e04c0f6c365e20af89dc89de82b66b4
yo <yo>
2021-03-18 21:50:16 +0300

    Complete task: Computer Sciense (Condition Determine Time)

--- Timofei/ComputerScienseConditionDetermineTime/ComputerScienseConditionDetermineTime/Program.cs
+++ Timofei/ComputerScienseConditionDetermineTime/ComputerScienseConditionDetermineTime/Program.cs
@@ -18,13 +18,14 @@ namespace ComputerScienseConditionDetermineTime
             
             int minutesInput = Int32.Parse(Console.ReadLine());
             
-            DetermineTime(minutesInput);
+            DetermineTimeMethodOne(minutesInput);
+            DetermineTimeMethodTwo(minutesInput);
         }
 
-        static void DetermineTime(int minutesInput)
+        static void DetermineTimeMethodOne(int minutesInput)
         {
             bool validationDataInput = ((0 <= minutesInput) && (minutesInput <= 59));
-
+            
             if (validationDataInput)
             {
                 if (0 <= minutesInput && minutesInput <= FIRST_QUARTER_HOUR)
@@ -40,6 +41,39 @@ namespace ComputerScienseConditionDetermineTime
                     Console.WriteLine("3/4 часа.");
                 }
                 if (THIRD_QUARTER_HOUR < minutesInput && minutesInput <= FOURTH_QUARTER_HOUR)
+                {
+                    Console.WriteLine("4/4 часа.");
+                }
+            }
+            else
+            {
+                Console.Write("Недопустимое число!");
+            }
+        }
+        
+        static void DetermineTimeMethodTwo(int minutesInput)
+        {
+            bool validationDataInput = ((0 <= minutesInput) && (minutesInput <= 59));
+            bool firstQuarterHour    = (0 <= minutesInput && minutesInput <= FIRST_QUARTER_HOUR);
+            bool secondQuarterHour   = (FIRST_QUARTER_HOUR < minutesInput && minutesInput <= SECOND_QUARTER_HOUR);
+            bool thirdQuarterHour    = (SECOND_QUARTER_HOUR < minutesInput && minutesInput <= THIRD_QUARTER_HOUR);
+            bool fourthQuarterHour   = (THIRD_QUARTER_HOUR < minutesInput && minutesInput <= FOURTH_QUARTER_HOUR);
+
+            if (validationDataInput)
+            {
+                if (firstQuarterHour)
+                {
+                    Console.WriteLine("1/4 часа.");
+                }
+                if (secondQuarterHour)
+                {
+                    Console.WriteLine("2/4 часа.");
+                }
+                if (thirdQuarterHour)
+                {
+                    Console.WriteLine("3/4 часа.");
+                }
+                if (fourthQuarterHour)
                 {
                     Console.WriteLine("4/4 часа.");
                 }
