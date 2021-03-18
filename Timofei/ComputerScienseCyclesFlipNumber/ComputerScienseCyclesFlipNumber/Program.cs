6bfadd3d0566ea40ba1f787b647300d57869b7a4
yo <yo>
2021-03-18 20:57:02 +0300

    Complete task: Computer Sciense (Cycles Flip Number)

---
+++ Timofei/ComputerScienseCyclesFlipNumber/ComputerScienseCyclesFlipNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ComputerScienseCyclesFlipNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dataInput = Int32.Parse(Console.ReadLine());
+            
+                FlipNumberMethodOne(dataInput);
+                FlipNumberMethodTwo(dataInput);
+        }
+
+        static void FlipNumberMethodOne(int dataInput)
+        {
+            int flipNumber = 0;
+            string sizeDataInput = Convert.ToString(dataInput);
+            
+            for (int i = 0; i < sizeDataInput.Length; i++)
+            {
+                flipNumber = dataInput % 10;
+                dataInput /= 10;
+                
+                Console.Write($"Метод 1.: {flipNumber}");
+            }
+        }
+
+        static void FlipNumberMethodTwo(int dataInput)
+        {
+            int flipNumber = 0;
+
+            while (dataInput > 0)
+            {
+                flipNumber = dataInput % 10;
+                dataInput /= 10;
+                
+                Console.Write($"Метод 2.: {flipNumber}");
+            }
+        }
+    }
+}
