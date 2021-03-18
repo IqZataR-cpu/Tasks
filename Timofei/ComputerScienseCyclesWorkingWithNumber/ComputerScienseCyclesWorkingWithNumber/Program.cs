10a2d9808c2ab9c2aa6398b4525cbe02ac36de22
yo <yo>
2021-03-18 20:13:59 +0300

    Complete task: Computer Sciense (Cycles Working With A Number)

---
+++ Timofei/ComputerScienseCyclesWorkingWithNumber/ComputerScienseCyclesWorkingWithNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ComputerScienseCyclesWorkingWithNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataInput = Console.ReadLine();
+            int counterEvenNumbers = 0; // 
+            int counterOddNumbers = 0; // 
+
+            for (int i = 0; i < dataInput.Length; i++)
+            {
+                if (Convert.ToChar(dataInput[i]) % 2 ==0)
+                {
+                    counterEvenNumbers++;
+                }
+                else
+                {
+                    counterOddNumbers++;
+                }
+            }
+            
+            Console.WriteLine($"Кол-во четных чисел: {counterEvenNumbers}");
+            Console.WriteLine($"Кол-во нечетных чисел: {counterOddNumbers}");
+        }
+    }
+}
