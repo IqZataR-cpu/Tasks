7ebce0999a96d707be855ea29cf7c05467cdb2ce
yo <yo>
2021-03-18 20:04:08 +0300

    Complete task: Cumputer Sciense (Validating User Imput)

---
+++ Timofei/ComputerScienseValidatingUserInput/ComputerScienseValidatingUserInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComputerScienseValidatingUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Не верно'.
+            
+            int  dataInput = Int32.Parse(Console.ReadLine());
+            bool validationDataInput = dataInput == 10;
+
+            if (validationDataInput)
+            {
+                Console.WriteLine("Верно.");
+            }
+            else
+            {
+                Console.WriteLine("Не верно.");
+            }
+        }
+    }
+}
