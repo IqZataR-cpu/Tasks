5a959f3557c8dd253866e314a72fc6a5842acaa0
yo <yo>
2021-03-18 21:45:29 +0300

    Complete task: Computer Sciense (Condition Determine Time)

---
+++ Timofei/ComputerScienseConditionDetermineTime/ComputerScienseConditionDetermineTime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ComputerScienseConditionDetermineTime
+{
+    class Program
+    {
+        //В переменной min лежит число от 0 до 59.
+        //Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+        
+        private const int FIRST_QUARTER_HOUR = 15;
+        private const int SECOND_QUARTER_HOUR = 30;
+        private const int THIRD_QUARTER_HOUR = 45;
+        private const int FOURTH_QUARTER_HOUR = 59;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минуты.");
+            
+            int minutesInput = Int32.Parse(Console.ReadLine());
+            
+            DetermineTime(minutesInput);
+        }
+
+        static void DetermineTime(int minutesInput)
+        {
+            bool validationDataInput = ((0 <= minutesInput) && (minutesInput <= 59));
+
+            if (validationDataInput)
+            {
+                if (0 <= minutesInput && minutesInput <= FIRST_QUARTER_HOUR)
+                {
+                    Console.WriteLine("1/4 часа.");
+                }
+                if (FIRST_QUARTER_HOUR < minutesInput && minutesInput <= SECOND_QUARTER_HOUR)
+                {
+                    Console.WriteLine("2/4 часа.");
+                }
+                if (SECOND_QUARTER_HOUR < minutesInput && minutesInput <= THIRD_QUARTER_HOUR)
+                {
+                    Console.WriteLine("3/4 часа.");
+                }
+                if (THIRD_QUARTER_HOUR < minutesInput && minutesInput <= FOURTH_QUARTER_HOUR)
+                {
+                    Console.WriteLine("4/4 часа.");
+                }
+            }
+            else
+            {
+                Console.Write("Недопустимое число!");
+            }
+        }
+    }
+}
