e18ead5fe2045b94c8dcce242eb63d7b466d33f7
IqZataR <Mikcha.y.12@gmail.com>
2021-03-20 19:17:09 +0300

    Fixme for timofei

--- Timofei/ComputerScienseCyclesFlipNumber/ComputerScienseCyclesFlipNumber/Program.cs
+++ Timofei/ComputerScienseCyclesFlipNumber/ComputerScienseCyclesFlipNumber/Program.cs
@@ -10,17 +10,26 @@ namespace ComputerScienseCyclesFlipNumber
         
         static void Main(string[] args)
         {
+            // сокращенная запись Int32 - int, используем ее дабы не получить пизды)))
             int dataInput = Int32.Parse(Console.ReadLine());
-            
+            // Ошибка табуляции, Python бы тебе такого не простил!
                 FlipNumberMethodOne(dataInput);
                 FlipNumberMethodTwo(dataInput);
         }
 
+        
+        // Этот метод полный отстой лучше его дропнуть
+        // Использование лишней переменной хотя можно было сразу в консоль выводить dataInput % 10.
+        // И для чего здесь конвертация в стринг? Чтобы узнать длину числа? - ну и рофлан конеш.
         static void FlipNumberMethodOne(int dataInput)
         {
             int flipNumber = 0;
             string sizeDataInput = Convert.ToString(dataInput);
             
+            
+            // А можно ебануться и написать так: 
+            // Console.WriteLine(Convert.ToString(dataInput).ToCharArray().Reverse().Aggregate("", (s, c) => s += c ));
+
             for (int i = 0; i < sizeDataInput.Length; i++)
             {
                 flipNumber = dataInput % 10;
@@ -30,6 +39,9 @@ namespace ComputerScienseCyclesFlipNumber
             }
         }
 
+        
+        // Это уже красивше, но зачем flipNumber???
+        // Ладно если бы ты в нем хранил полное перевернутое число, ну а так смысл??
         static void FlipNumberMethodTwo(int dataInput)
         {
             int flipNumber = 0;
