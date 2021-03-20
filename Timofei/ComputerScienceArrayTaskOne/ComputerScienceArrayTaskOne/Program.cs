3979f29575195ab86bbcfc0e41d176a017b60ab1
IqZataR <Mikcha.y.12@gmail.com>
2021-03-20 18:49:55 +0300

    Fixme for timofei

--- Timofei/ComputerScienceArrayTaskOne/ComputerScienceArrayTaskOne/Program.cs
+++ Timofei/ComputerScienceArrayTaskOne/ComputerScienceArrayTaskOne/Program.cs
@@ -9,9 +9,10 @@ namespace ComputerScienceArrayTaskOne
             //Вывести элементы числового массива, которые больше, чем элементы, стоящие перед ними.
             
             int[] arrayInput = new[] {3, 9, 8, 4, 5, 1, 5, 6};
-           
+
             for (int i = 1; i < arrayInput.Length; i++)
-            {
+            {   
+                // забыл "-" с двух сторон запятыми выделить.
                 if (arrayInput[i-1] < arrayInput[i])
                 { 
                     Console.Write($"{arrayInput[i]} "); ;
@@ -20,7 +21,10 @@ namespace ComputerScienceArrayTaskOne
         }
     }
 }
+// Зачем тут закомментированный код??? УДАЛИТЬ!!!
 
+
+// 
 // if (step != arrayInput.Length)
 // {
 //     if (arrayInput[i] < arrayInput[step])
