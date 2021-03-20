3e40fd1a41e5773cd49d36822c1d7bfd3c787d1b
IqZataR <Mikcha.y.12@gmail.com>
2021-03-20 19:23:57 +0300

    Fixme for timofei

--- Timofei/ComputerScienseValidatingUserInput/ComputerScienseValidatingUserInput/Program.cs
+++ Timofei/ComputerScienseValidatingUserInput/ComputerScienseValidatingUserInput/Program.cs
@@ -8,6 +8,8 @@ namespace ComputerScienseValidatingUserInput
         {
             // Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Не верно'.
             
+            // Все ок, но Int32 = int, исправь,
+            // вынеси bool переменную в функцию, либо просто убери ее...
             int  dataInput = Int32.Parse(Console.ReadLine());
             bool validationDataInput = dataInput == 10;
 
