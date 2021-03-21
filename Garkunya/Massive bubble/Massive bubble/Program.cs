306389eb3b6ff6ab9500ca6a02c4c4a6cf32f8eb
Garkunya <freshtime.dom1no3@gmail.com>
2021-03-21 18:49:54 +0300

    Massive Bubble

---
+++ Garkunya/Massive bubble/Massive bubble/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Massive_bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] someArray = new int[] {4, 7, 2, 3, 9, 8, 0, 1, 5, 6}; // заранее известный массив
+            int temp; //переменная для сравнения
+
+            for (int i = 0; i < someArray.Length; i++)
+            {
+                for (int j = i + 1; j < someArray.Length; j++)
+                {
+                    if (someArray[i] > someArray[j]) //сравниваем соседние переменные и меняем их местами
+                    {                               // в случае успеха
+                        temp = someArray[i];
+                        someArray[i] = someArray[j];
+                        someArray[j] = temp;
+                    }                   
+                }            
+            }
+            
+            for (int i = 0; i < someArray.Length; i++) //выводим массив
+            {
+                Console.Write(someArray[i] + " ");
+            } 
+
+        }
+    }
+}
