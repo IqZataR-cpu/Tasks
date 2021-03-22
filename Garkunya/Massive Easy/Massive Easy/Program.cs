4ad6fb5a9e26101bcf4eda9c914e29ab58f6224c
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-03-22 11:33:44 +0300

    Fixme for Garkunya

--- Garkunya/Massive Easy/Massive Easy/Program.cs
+++ Garkunya/Massive Easy/Massive Easy/Program.cs
@@ -10,34 +10,39 @@ using Microsoft.Extensions.Logging;
 
 namespace Massive_Easy
 {
+    // Решил правильно, но алгоритм откровенно неверный, поэтому переделай на myArray[i - 1]
     public class Program
     {
         public static void Main(string[] args)
         {
             Random rand = new Random();
 
-            Console.Write("Введите количество элементов массива: "); //Вводим количество элементов
+            Console.Write("Введите количество элементов массива: "); //Вводим количество элементов FUCK MOY MOZG!!!
             int n = int.Parse(Console.ReadLine());
-            int[] myArray= new int[n-1]; //Размер массива
+            // Не выделил пробелами знаки!
+            int[] myArray= new int[n-1]; //Размер массива FUCK MOY MOZG!!!
 
             for (int i = 0; i<myArray.Length; i++) 
             {
-                myArray[i] = rand.Next(0,100); //Заполняем случайными числами
-                Console.Write(myArray[i] + " "); // И выводим его
+                myArray[i] = rand.Next(0,100); //Заполняем случайными числами FUCK MOY MOZG!!!
+                Console.Write(myArray[i] + " "); // И выводим его FUCK MOY MOZG!!! FUCK MOY MOZG!!! FUCK MOY MOZG!!! FUCK MOY MOZG!!!
             }
             
-            Console.WriteLine(); //Отступ, чтобы разделить от результата
+            Console.WriteLine(); //Отступ, чтобы разделить от результата FUCK MOY MOZG!!! FUCK MOY MOZG!!!
             
-            int firstNumber = myArray[0]; //Присваиваю переменной 1-ое значение для сравнения
+            // зачем эта переменная, если можно проверять myArray[i - 1]???? Лишнее место в памяти!
+            int firstNumber = myArray[0]; //Присваиваю переменной 1-ое значение для сравнения FUCK MOY MOZG!!! 
             
+            // цикл можно стартануть с i = 1 и не будет выхода за пределы массива, если использовать myArray[i - 1]
             for (int i = 0; i < myArray.Length; i++)
             {
-                if (firstNumber<myArray[i]) //если больше, то обновление и вывод
+                // не выделил знак сравнения пробелами
+                if (firstNumber<myArray[i]) //если больше, то обновление и вывод FUCK MOY MOZG!!!
                 {
                     firstNumber = myArray[i];
                     Console.WriteLine(firstNumber);
                 }
-                else //если меньше, то только замена переменной сравнения
+                else //если меньше, то только замена переменной сравнения FUCK MOY MOZG!!!
                 {
                     firstNumber = myArray[i];
                 }
