39c55bf0fe35695e16f0052c80a66b5f72055d0d
Mikuzen <Mikuzen@github.com>
2021-03-22 11:13:50 +0300

    Fix two methods sort for array

--- Timofei/ComputerScienceArraySortByInserts/ComputerScienceArraySortByInserts/Program.cs
+++ Timofei/ComputerScienceArraySortByInserts/ComputerScienceArraySortByInserts/Program.cs
@@ -6,10 +6,18 @@ namespace ComputerScienceArraySortByInserts
     {
         static void Main(string[] args)
         {
+            // Для чего здесь ты делаешь массив с данными, если
+            // Если ты вызываешь метод, который заполнит массив заново
+            // Другими значениями
             int[] arrayNumber = new int[] {40, 30, 10, 70, 50, 20, 60};
             
             InsertNumberByArray(arrayNumber);
-            
+
+            // Круто, что ты сделал две сортировки сразу, но
+            // Когда вызывается сортировка вставками, ей если
+            // Что передается уже отсортированный массив, так что
+            // Суть сортировки пропадает, либо добавь ещё один массив
+            // Либо по отдельности реализуй задачи
             SortByBooble(arrayNumber);
             Console.WriteLine();
             SortByInsert(arrayNumber);
@@ -43,16 +51,22 @@ namespace ComputerScienceArraySortByInserts
                 arrayNumber[j] = step;
             }
 
+            // Для вывода сделай функцию или метод, чтобы не было
+            // Дубляжа кода и перполнение ответственности у методов
             for (int i = 0; i < arrayNumber.Length; i++)
             {
                 Console.Write($"{arrayNumber[i]} ");
             }
         }
 
+        // Метод сортировки пузырька реализован неверно,
+        // Суть метода заключается в проверке двух соседних
+        // Элементов массива, а не так как у тебя реализовано
         static void SortByBooble(int[] arrayNumber)
         {
             Console.Write("Сортировка методом пузырька: ");
-            
+
+            // Название переменной не отражает суть испрользование переменной
             int step = arrayNumber[0];
             
             for (int i = 0; i < arrayNumber.Length; i++)
@@ -67,7 +81,9 @@ namespace ComputerScienceArraySortByInserts
                     }
                 }
             }
-            
+
+            // Для вывода сделай функцию или метод, чтобы не было
+            // Дубляжа кода и перполнение ответственности у методов
             for (int i = 0; i < arrayNumber.Length; i++)
             {
                 Console.Write($"{arrayNumber[i]} ");
