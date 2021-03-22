5cc81dfc4721d528c6ef44b1fca616cc1e709fa4
Garkunya <freshtime.dom1no3@gmail.com>
2021-03-22 22:33:10 +0300

    Massive Medium 2 update

--- Garkunya/Massive Medium 2/Massive Medium 2/Program.cs
+++ Garkunya/Massive Medium 2/Massive Medium 2/Program.cs
@@ -10,39 +10,29 @@ namespace Massive_Medium_2
             
             Console.Write("Введите количество элементов массива: ");
             
-            int n = int.Parse(Console.ReadLine()); //вводим количество элементов
-            int[] myArray = new int[n]; //создаем массив с n размером
-            int countNumber = 0;  //переменная, которая будет хранить кол-во элементов
-            float sumNumber = 0; // переменная, которая будет хранить сумму элементов
+            int n = int.Parse(Console.ReadLine());
+            int[] myArray = new int[n];
+            int countNumber = 0;
+            float sumNumber = 0;
 
-            for (int i = 0; i < myArray.Length; i++) //заполняем массив случайными числами от 0 до 100
+            for (int i = 0; i < myArray.Length; i++)
             {
                 myArray[i] = rnd.Next(0, 100);
-                // Не выделил знак "+" пробелами
-                Console.Write(myArray[i]+" ");
+                Console.Write(myArray[i] + " ");
             }
             
             Console.WriteLine(); 
 
-            for (int i = 0; i < myArray.Length; i++) //проверяем число на условие
+            for (int i = 0; i < myArray.Length; i++)
             {
-                // Не выделил знаки пробелами
-                // Нет смысла разбивать на 2 if,
-                // можно через && написать два условия в одном if
-                if (myArray[i]%2==0)
+                if (myArray[i] % 2 == 0 && i % 2 != 0)
                 {
-                    if (i%2 != 0)
-                    {
-                        sumNumber += myArray[i];
-                        countNumber++;
-                    }
+                    sumNumber += myArray[i];
+                    countNumber++;
                 }
             }
-            // Нет смысла в этой операции, 
-            // можно в консольный вывод сразу вывести sumNumber / countNumber
-            sumNumber = sumNumber / countNumber;
-            
-            Console.WriteLine($"Среднее арифметическое {countNumber} чисел = {sumNumber}"); //выводим результат
+
+            Console.WriteLine($"Среднее арифметическое {countNumber} чисел = {sumNumber/countNumber}");
         }
     }
 }
