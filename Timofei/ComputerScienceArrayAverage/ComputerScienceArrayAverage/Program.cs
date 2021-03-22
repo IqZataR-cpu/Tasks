0f9fc0c053838a72b5d13c07a052b583729eade5
Mikuzen <Mikuzen@github.com>
2021-03-22 10:14:40 +0300

    Fix task of average numbers

--- Timofei/ComputerScienceArrayAverage/ComputerScienceArrayAverage/Program.cs
+++ Timofei/ComputerScienceArrayAverage/ComputerScienceArrayAverage/Program.cs
@@ -15,9 +15,9 @@ namespace ComputerScienceArrayAverage
         static int[] InsertNumberInArray(int[] arrayInput)
         {
             Random randomNumber = new Random();
-            
+
             Console.Write("Массив: ");
-            
+
             for (int i = 0; i < arrayInput.Length; i++)
             {
                 arrayInput[i] = randomNumber.Next(100);
@@ -29,11 +29,15 @@ namespace ComputerScienceArrayAverage
 
         static void AverageNumbersArray(int[] arrayInput)
         {
+            // sumAverage лучше сделать типа double or float
+            // Потому что оно может быть дробным
             int sumAverage = 0;
             int count = 0;
-            
+            // Не совсем корректная фраза здесь, потому что
+            // Человек видит нечетные числа, а на выводе в итоге
+            // Они оказываются четными
             Console.Write($"\n Нечетные числа: ");
-            
+
             for (int i = 0; i < arrayInput.Length; i++)
             {
                 if (i % 2 != 0 && arrayInput[i] % 2 == 0)
