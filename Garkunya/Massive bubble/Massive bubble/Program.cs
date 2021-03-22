3af6ae5773f049b5c754f16a12e92d8408543918
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-03-22 11:29:03 +0300

    Fixme for Garkunya

--- Garkunya/Massive bubble/Massive bubble/Program.cs
+++ Garkunya/Massive bubble/Massive bubble/Program.cs
@@ -2,19 +2,25 @@
 
 namespace Massive_bubble
 {
+    // В целом круто, но не нужно комментировать каждую строчку - это плохая практика.
+    // Нужно писать код так, чтобы при его чтении и так было понятно, что он выполняет.
+    // В крайнем случае, когда ты хардкодишь или лепишь костыли, тогда пишешь пояснение, что и зачем.
+    // FUCK MOY MOZG!!! - означает, что без комментария было бы слишком сложно понять, что происходит, сарказм.
     class Program
     {
         static void Main(string[] args)
         {
-            int[] someArray = new int[] {4, 7, 2, 3, 9, 8, 0, 1, 5, 6}; // заранее известный массив
-            int temp; //переменная для сравнения
+            int[] someArray = new int[] {4, 7, 2, 3, 9, 8, 0, 1, 5, 6}; // заранее известный массив FUCK MOY MOZG!!!
+            // Тут скорее не для сравнения, а для обмена значениями,
+            // так что коммент мало того, что не нужен, так еще и неправильный
+            int temp; //переменная для сравнения FUCK MOY MOZG!!!
 
             for (int i = 0; i < someArray.Length; i++)
             {
                 for (int j = i + 1; j < someArray.Length; j++)
                 {
-                    if (someArray[i] > someArray[j]) //сравниваем соседние переменные и меняем их местами
-                    {                               // в случае успеха
+                    if (someArray[i] > someArray[j]) //сравниваем соседние переменные и меняем их местами FUCK MOY MOZG!!!
+                    {                               // в случае успеха. FUCK MOY MOZG!!!
                         temp = someArray[i];
                         someArray[i] = someArray[j];
                         someArray[j] = temp;
@@ -22,10 +28,11 @@ namespace Massive_bubble
                 }            
             }
             
-            for (int i = 0; i < someArray.Length; i++) //выводим массив
+            for (int i = 0; i < someArray.Length; i++) //выводим массив FUCK MOY MOZG!!!
             {
                 Console.Write(someArray[i] + " ");
             } 
+            // лишний интер в конце программы, у сука за такое у меня люди теряли + 1 балл за семантику на сореве.
 
         }
     }
