359ebd630ddf055598649cb717c2d94bbf587101
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-03-22 11:50:54 +0300

    Fixme for Garkunya

--- Garkunya/Test1/Test1/Program.cs
+++ Garkunya/Test1/Test1/Program.cs
@@ -7,18 +7,27 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            Worker[] Workers = new Worker[] {new Worker("Михаил", "Будяков", 3000, 20), new Worker("Александр","Куншин",1500,20), new Worker("Лихачев","Артем",1000,10)};
+            // Сделай переносы, не нужно писать код одной строкой, это не ВБА!!! Пример покажу на будущее -> ниже как правильно.
+            Worker[] Workers = new Worker[] {
+                new Worker("Михаил", "Будяков", 3000, 20),
+                // не выделил знаки пробелами
+                new Worker("Александр","Куншин",1500,20),
+                new Worker("Лихачев","Артем",1000,10)
+            };
 
-            Console.WriteLine("Все работники: ");
+            Console.WriteLine("Все работники: "); // Можно отделить enter, субъективно
             for (int i = 0; i < Workers.Length; i++)
             {
+                // не выделил знаки пробелами
                 Console.WriteLine($"Работник{i+1}: ");
                 Workers[i].ShowWorkers();
                 Console.WriteLine("\n");
             }
             
             Console.Write("Введите номер работника, по которому нужна информация: ");
+            // Почему не int.Parse(), тебе действительно нужен для этой операции целый класс Convert?
             int numberWorker = Convert.ToInt32(Console.ReadLine());
+            // не выделил знаки пробелами
             Workers[numberWorker-1].Salary();
         }
         
@@ -38,13 +47,26 @@ namespace Test1
             Days = days;
         }
 
+        // Не корректное название метода, логично было бы назвать Info или Show,
+        // по названию класса и так понятно что это Worker.
+        // Но самое главное почему WorkerSSS, а не Worker,
+        // ты же инфу только по одному выводишь за один вызов метода.
         public void ShowWorkers()
         {
-            Console.Write("Имя работника = " + Name + "\nФамилия = "+ Surname + "\nСтавка в день = "+ Rate + "\nКоличество дней = "+ Days);
+            // Лучше \n выделить enter, и использовать интерполяцию строк, пример ниже
+            Console.Write(
+                $"Имя работника = { Name } \n" +
+                $"Фамилия = { Surname } \n" +
+                $"Ставка в день = { Rate } \n" +
+                $"Количество дней = { Days }"
+            );
         }
 
         public void Salary()
         {
+            // Лишнее место в памяти, 
+            // по названию метода и так понятно что будет выведено,
+            // поэтому просто в консоль выводим Rate * Days, вместо Salary
             int Salary = Rate * Days;
             Console.WriteLine($"Зарплата данного работника составляет: {Salary}");
         }
