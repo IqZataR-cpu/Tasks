35fb62e97e4df21be2346a59322ae4e7ecf9cbfb
Garkunya <freshtime.dom1no3@gmail.com>
2021-03-23 22:10:34 +0300

    Cycle Easy 1 update

--- Garkunya/Cycle Easy 1/Cycle Easy 1/Program.cs
+++ Garkunya/Cycle Easy 1/Cycle Easy 1/Program.cs
@@ -4,26 +4,19 @@ namespace Cycle_Easy_1
 {
     class Program
     {
-        // Комментарии подобного вида как у тебя это излишне!!!
-        // Удали их просто или буду писать FUCK MOY MOZG!!! =) (=
-        // UPD. Решил сам их удалить, но запомни на будущее!
         static void Main(string[] args)
         {
             Console.Write("Введите число: ");
-            int n = int.Parse(Console.ReadLine());
+            int inputValue = int.Parse(Console.ReadLine());
             int temp = 0;
-            // не выделил пробелами
-            int countRemain=0;
-            // не выделил пробелами
-            int countNoRemain=0;
+            int countRemain = 0;
+            int countNoRemain = 0;
             
-            // не выделил пробелами
-            while (n>0)
+            while (inputValue > 0)
             {
-                temp = n % 10;
-                n /= 10; 
-                // не выделил пробелами
-                if (temp%2==0)
+                temp = inputValue % 10;
+                inputValue /= 10; 
+                if (temp %  2== 0)
                 {
                     countRemain++;
                 }
