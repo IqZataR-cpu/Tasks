147bca1870784c1c3d9003452d70bec59e0e8969
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-03-23 12:03:43 +0300

    Fixme for Artem

--- Artem/MultiplicationMatrix/MultiplicationMatrix/Program.cs
+++ Artem/MultiplicationMatrix/MultiplicationMatrix/Program.cs
@@ -32,23 +32,37 @@ namespace MultiplicationMatrix
             }
             else
             {
+                // Когда переносишь строки пробел между ними лучше оставлять в конце, а не в начале строки.
                 Console.WriteLine("Умножение матриц не возможно, так как количество столбцов"+
                          "  матрицы А не равно количеству строк в матрице В");
             }          
         }
 
+        // Старайся делать названия параметров не под частный случай, а под все подобные случаи.
+        // В целом слово number здесь не нужно, можно просто columnsMatrixA(или columnsFirstMatrix)...
         static bool canMultiplicate (int numberOfColumnsMatrixA, int numberOfRowsMatrixB)
         {
+            // Эта переменная здесь не нужна, смысл и так понятен.
+            // Для этого мы и создали функцию canMultiplicate
             bool isEqualsTwoNumbers = numberOfColumnsMatrixA == numberOfRowsMatrixB;
 
+            // Это можно заменить одной строчкой return isEqualsTwoNumbers; 
+            // Смысл сохраниться оно вернет либо false, либо true =)
             if (isEqualsTwoNumbers)
             {
                 return true;
             }
 
             return false;
+            
+            // Таким образом этот метод можно сократить до одной строки
+            // return numberOfColumnsMatrixA == numberOfRowsMatrixB;
         }
 
+        // Здесь не следовало передавать результирующую матрицу,
+        // стоило просто создать ее внутри метода и вернуть ее
+        // (массив - ссылочный тип, так что проблем с производительностью нет)
+        // Ты бы вернул просто ссылку на массив.
         static int[,] CompletionMatrix(int [,] array, int numberOfRows, int numberOfColumns)
         {
             Random randomNumber = new Random();
@@ -64,6 +78,8 @@ namespace MultiplicationMatrix
             return array;
         }
 
+        // numberOfRows и Columns можно получить с помощью Length.
+        // Та же проблема с результирующей матрицей.
         static int[,] MultiplicateMatrix (int[,] matrixC, int[,] matrixA, int[,] matrixB, int numberOfRows, int numberOfColumns)
         {
             for (int i = 0; i <= numberOfRows - 1; i++)
@@ -79,13 +95,18 @@ namespace MultiplicationMatrix
 
             return matrixC;
         }
-
+        
+        // Можно просто передавать matrix, по названию метода и так понятно, что она будет распечатана,
+        // Ширину и длину матрицы можно взять прямиком из массива. Length
+        // Еще можешь попытаться сделать метод универсальным для любой мерности,
+        // то есть если была бы 3х, 4х... размерная матрица.
         static void PrintMatrix (int[,] printedMatrix, int numberOfRows, int numberOfColumns)
         {
             Console.WriteLine("\n Матрица:");
 
             for (int i = 0; i <= numberOfRows - 1; i++)
             {
+                // Я бы лучше после цикла сделал перенос строки, а не перед.
                 Console.WriteLine();
 
                 for (int j = 0; j <= numberOfColumns - 1; j++)
