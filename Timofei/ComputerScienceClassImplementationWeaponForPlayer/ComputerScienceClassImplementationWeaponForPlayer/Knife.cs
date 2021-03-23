d14108d2f6cced6a2bd098fedd1dc8e2ba79b18b
yo <yo>
2021-03-23 23:16:10 +0300

    Complete task: Computer Science Matrix Max Element

---
+++ Timofei/ComputerScienceClassImplementationWeaponForPlayer/ComputerScienceClassImplementationWeaponForPlayer/Knife.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ComputerScienceClassImplementationWeaponForPlayer
+{
+    public class Knife : Weapon
+    {
+        private const int DAMAGE = 35;
+        
+        public static void Print()
+        {
+            Console.WriteLine($"Урон: {DAMAGE} \n");
+        }
+        
+        public Knife(int damage) : base(damage)
+        {
+        }
+    }
+}
