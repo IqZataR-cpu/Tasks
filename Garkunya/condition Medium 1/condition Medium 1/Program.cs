2c5c729d44fd6ca244c2113edd43964ab8444b2b
Garkunya <freshtime.dom1no3@gmail.com>
2021-03-23 22:08:41 +0300

    Condition Medium 1 update

--- Garkunya/condition Medium 1/condition Medium 1/Program.cs
+++ Garkunya/condition Medium 1/condition Medium 1/Program.cs
@@ -8,23 +8,23 @@ namespace condition_Medium_1
         {
             Random rnd = new Random();
 
-            int min = rnd.Next(0, 59); //получаем случайное число
+            int minuteValue = rnd.Next(0, 59);
 
-            if (min>=0 && min <15) //проверяем условие и выводим
+            if (minuteValue >= 0 && minuteValue < 15)
             {
-                Console.Write($"{min} минут находятся в 1-ой четверти часа");
+                Console.Write($"{ minuteValue } минут находятся в 1-ой четверти часа");
             }
-            else if (min>=15 && min <30)
+            else if (minuteValue >= 15 && minuteValue < 30)
             {
-                Console.Write($"{min} минут находятся в 2-ой четверти часа");
+                Console.Write($"{ minuteValue } минут находятся в 2-ой четверти часа");
             }
-            else if (min>=30 && min <45)
+            else if (minuteValue >= 30 && minuteValue < 45)
             {
-                Console.Write($"{min} минут находятся в 3-ой четверти часа");
+                Console.Write($"{ minuteValue } минут находятся в 3-ой четверти часа");
             }
             else
             {
-                Console.Write($"{min} минут находятся в 4-ой четверти часа");
+                Console.Write($"{ minuteValue } минут находятся в 4-ой четверти часа");
             }
         }
     }
