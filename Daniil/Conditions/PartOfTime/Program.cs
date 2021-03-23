90b853af62c8d00856b06993a1aa9bfbb892de37
DESKTOP-3OGPFAT\danii <daniil.dg.galkin@gmail.com>
2021-03-23 12:08:14 +0300

    Complete input validation and task called part of time

---
+++ Daniil/Conditions/PartOfTime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionsTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = -1;
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Введите число");
+                min = Convert.ToInt32(Console.ReadLine());
+
+                if (min >= 0 && min <= 59) {check = false;}
+            }
+
+
+            if (min > 0 && min <= 15) 
+            {
+                Console.WriteLine("Число входит в первую четверть часа"); 
+            }
+            else if (min > 15 && min <= 30) 
+            {
+                Console.WriteLine("Число входит в вторую четверть часа"); 
+            }
+            else if (min > 30 && min <= 45) 
+            {
+                Console.WriteLine("Число входит в третью четверть часа"); 
+            }
+            else 
+            {
+                Console.WriteLine("Число входит в четвертую четверть часа");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
