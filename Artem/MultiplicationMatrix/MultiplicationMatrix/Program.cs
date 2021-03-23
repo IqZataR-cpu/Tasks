882b22d36f8934628e407ff6ad56caf1ca9c452f
Mikuzen <Mikuzen@github.com>
2021-03-23 19:14:19 +0300

    Fixed multiplicate two matrix

--- Artem/MultiplicationMatrix/MultiplicationMatrix/Program.cs
+++ Artem/MultiplicationMatrix/MultiplicationMatrix/Program.cs
@@ -21,54 +21,33 @@ namespace MultiplicationMatrix
 
             if (canMultiplicate(numberOfColumnsMatrixA, numberOfRowsMatrixB))
             {
-                CompletionMatrix(matrixA, numberOfRowsMatrixA, numberOfColumnsMatrixA);
-                PrintMatrix(matrixA, numberOfRowsMatrixA, numberOfColumnsMatrixA);
+                matrixA = CompletionMatrix(numberOfRowsMatrixA, numberOfColumnsMatrixA);
+                PrintMatrix(matrixA);
 
-                CompletionMatrix(matrixB, numberOfRowsMatrixB, numberOfColumnsMatrixB);
-                PrintMatrix(matrixB, numberOfRowsMatrixB, numberOfColumnsMatrixB);
+                matrixB = CompletionMatrix(numberOfRowsMatrixB, numberOfColumnsMatrixB);
+                PrintMatrix(matrixB);
 
-                MultiplicateMatrix(matrixC, matrixA, matrixB, numberOfRowsMatrixA, numberOfColumnsMatrixB);
-                PrintMatrix(matrixC, numberOfRowsMatrixA, numberOfColumnsMatrixB);
+                matrixC = MultiplicateMatrix( matrixA, matrixB);
+                PrintMatrix(matrixC);
             }
             else
             {
-                // Когда переносишь строки пробел между ними лучше оставлять в конце, а не в начале строки.
-                Console.WriteLine("Умножение матриц не возможно, так как количество столбцов"+
-                         "  матрицы А не равно количеству строк в матрице В");
+                Console.WriteLine("Умножение матриц не возможно, так как количество столбцов "+
+                         "матрицы А не равно количеству строк в матрице В");
             }          
         }
 
-        // Старайся делать названия параметров не под частный случай, а под все подобные случаи.
-        // В целом слово number здесь не нужно, можно просто columnsMatrixA(или columnsFirstMatrix)...
-        // Можно даже просто columns и rows.
-        static bool canMultiplicate (int numberOfColumnsMatrixA, int numberOfRowsMatrixB)
+        static bool canMultiplicate (int ColumnsMatrixA, int RowsMatrixB)
         {
-            // Эта переменная здесь не нужна, смысл и так понятен.
-            // Для этого мы и создали функцию canMultiplicate
-            bool isEqualsTwoNumbers = numberOfColumnsMatrixA == numberOfRowsMatrixB;
-
-            // Это можно заменить одной строчкой return isEqualsTwoNumbers; 
-            // Смысл сохраниться оно вернет либо false, либо true =)
-            if (isEqualsTwoNumbers)
-            {
-                return true;
-            }
-
-            return false;
-            
-            // Таким образом этот метод можно сократить до одной строки
-            // return numberOfColumnsMatrixA == numberOfRowsMatrixB;
+            return ColumnsMatrixA == RowsMatrixB;
         }
 
-        // Здесь не следовало передавать результирующую матрицу,
-        // стоило просто создать ее внутри метода и вернуть ее
-        // (массив - ссылочный тип, так что проблем с производительностью нет)
-        // Ты бы вернул просто ссылку на массив.
-        static int[,] CompletionMatrix(int [,] array, int numberOfRows, int numberOfColumns)
+        static int[,] CompletionMatrix(int numberOfRows, int numberOfColumns)
         {
             Random randomNumber = new Random();
+            int[,] array = new int[numberOfRows, numberOfColumns];
 
-            for (int i = 0; i <= numberOfColumns - 1; i++)
+            for (int i = 0; i <= numberOfRows - 1; i++)
             {
                 for (int j = 0; j <= numberOfColumns - 1; j++)
                 {
@@ -79,15 +58,15 @@ namespace MultiplicationMatrix
             return array;
         }
 
-        // numberOfRows и Columns можно получить с помощью Length.
-        // Та же проблема с результирующей матрицей.
-        static int[,] MultiplicateMatrix (int[,] matrixC, int[,] matrixA, int[,] matrixB, int numberOfRows, int numberOfColumns)
+        static int[,] MultiplicateMatrix (int[,] matrixA, int[,] matrixB)
         {
-            for (int i = 0; i <= numberOfRows - 1; i++)
+            int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+            for (int i = 0; i <= matrixC.GetLength(0) - 1; i++)
             {
-                for (int j = 0; j <= numberOfColumns - 1; j++)
+                for (int j = 0; j <= matrixC.GetLength(1) - 1; j++)
                 {
-                    for (int k = 0; k <= numberOfRows - 1; k++)
+                    for (int k = 0; k <= matrixA.GetLength(1) - 1; k++)
                     {
                         matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
                     }                  
@@ -101,19 +80,18 @@ namespace MultiplicationMatrix
         // Ширину и длину матрицы можно взять прямиком из массива. Length
         // Еще можешь попытаться сделать метод универсальным для любой мерности,
         // то есть если была бы 3х, 4х... размерная матрица.
-        static void PrintMatrix (int[,] printedMatrix, int numberOfRows, int numberOfColumns)
+        static void PrintMatrix (int[,] matrix)
         {
             Console.WriteLine("\n Матрица:");
 
-            for (int i = 0; i <= numberOfRows - 1; i++)
-            {
-                // Я бы лучше после цикла сделал перенос строки, а не перед.
-                Console.WriteLine();
-
-                for (int j = 0; j <= numberOfColumns - 1; j++)
+            for (int i = 0; i <= matrix.GetLength(0) - 1; i++)
+            {             
+                for (int j = 0; j <= matrix.GetLength(1) - 1; j++)
                 {
-                    Console.Write($"{printedMatrix[i, j]} ");
+                    Console.Write($"{matrix[i, j]} ");
                 }
+
+                Console.WriteLine();
             }
         }
     }
