53f920243dcd18dcf5925d13891f79ceb8d5c306
yo <yo>
2021-03-24 14:19:19 +0300

    Fix task: Computer Science Matrix MaxElement of the Diagonal

--- Timofei/ComputerScienceMatrixMaxElement/ComputerScienceMatrixMaxElement/Program.cs
+++ Timofei/ComputerScienceMatrixMaxElement/ComputerScienceMatrixMaxElement/Program.cs
@@ -11,12 +11,29 @@ namespace ComputerScienceMatrixMaxElement
         {
             int[,] matrix = new int[sizeLine, sizeColumn];
 
+            matrix = InsertToMatrix(matrix);
+            
             PrintMatrix(matrix);
-            SearchMaxElement(matrix);
+            SearchMaxElementDiagonal(matrix);
         }
 
         // Помним про разделение ответственностей, здесь их 2,
         // то есть название функции не соответствует действительности
+        static int[,] InsertToMatrix(int[,] matrix)
+        {
+            Random rnd = new Random();
+            
+            for (int i = 0; i < sizeLine; i++)
+            {
+                for (int j = 0; j < sizeColumn; j++)
+                {
+                    matrix[i, j] = rnd.Next(99);
+                }
+            }
+
+            return matrix;
+        }
+
         static void PrintMatrix(int[,] matrix)
         {
             Random rnd = new Random();
@@ -35,14 +52,14 @@ namespace ComputerScienceMatrixMaxElement
         }
 
         // Тут я бы тоже указал, что ищется именно максимальный диагональный элемент.
-        static void SearchMaxElement(int[,] matrix)
+        static void SearchMaxElementDiagonal(int[,] matrix)
         {
             int maxElement = matrix[0, 0];
             int indexI = 0, indexJ = 0;
             
             // Такие циклы конечно круто, но старайся ими не пользоваться,
             // просто знай, что такое существует, но это не любят обычно
-            for (int i = 1, j = 1; i < sizeColumn; i++, j = i)
+            for (int i = 0, j = 1; i < sizeColumn; i++, j = i)
             {
                 if (matrix[i, j] > maxElement)
                 {
@@ -65,7 +82,7 @@ namespace ComputerScienceMatrixMaxElement
             }
 
             // то есть пробелы между знаками в коде мы ставим, а тут нет?
-            Console.WriteLine($"Индекс максимального элемента двух диагоналей:[{indexI},{indexJ}] = {maxElement}");
+            Console.WriteLine($"Индекс максимального элемента двух диагоналей:[{indexI}, {indexJ}] = {maxElement}");
         }
     }
 }
