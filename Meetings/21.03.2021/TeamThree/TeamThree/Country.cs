d0b15012087ccb85b145bad6bcacd15c57bf3d38
DESKTOP-3OGPFAT\danii <daniil.dg.galkin@gmail.com>
2021-03-25 20:59:29 +0300

    Add city, country, Region

---
+++ Meetings/21.03.2021/TeamThree/TeamThree/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TeamThree
+{
+    public class Country
+    {
+        private int _id;
+        private string _name;
+        private string _createdAt;
+        
+        public Country(int id, string name, string crt_date)
+        {
+            _id = id;
+            _name = name;
+            _createdAt = crt_date;
+        }
+
+        public void PrintStates()
+        {
+            Console.WriteLine($"Id:{_id}, Name:{_name}, Created At:{_createdAt}");
+        }
+    }
+}
---
+++ Meetings/21.03.2021/TeamThree/TeamThree/Region.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TeamThree
+{
+    public class Region
+    {
+        private int _id;
+        private int _countryId;
+        private string _name;
+        private string _createdAt;
+        
+        public Region(int id, int country_id, string name, string crt_date)
+        {
+            _id = id;
+            _countryId = country_id;
+            _name = name;
+            _createdAt = crt_date;
+        }
+
+        public void PrintStates()
+        {
+            Console.WriteLine($"Id:{_id}, Region_id:{_countryId}, Name:{_name}, Created At:{_createdAt}");
+        }
+    }
+}
