92e6a06d636ce5411fa67c40c7c3491e1127d674
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-03-27 23:26:43 +0300

    Fixme Timofei

--- Timofei/ComputerScienceConditionTaskThree/ComputerScienceConditionTaskThree/Program.cs
+++ Timofei/ComputerScienceConditionTaskThree/ComputerScienceConditionTaskThree/Program.cs
@@ -15,15 +15,18 @@ namespace ComputerScienceConditionTaskThree
             double result = EquationSolution(inputA);
             
             CompareSolution(inputB, result);
+            // Здесь соответственно вывести на экран в зависимости от условия.
         }
 
         static void CompareSolution(int inputB, double result)
         {
             Console.WriteLine(inputB == result);
+            // тут лучше просто сравнение inputB == result
         }
         
         static double EquationSolution(int x)
         {
+            // Скобки не нужны
             return (5 * Math.Pow(x, 2) - 7 * x + 2);
         }
     }
