e3d7481db4d6c38bf7600293e9e095de7a2b0ee2
yo <yo>
2021-03-27 23:32:43 +0300

    fix ComputerScienceConditionTaskThree

--- Timofei/ComputerScienceConditionTaskThree/ComputerScienceConditionTaskThree/Program.cs
+++ Timofei/ComputerScienceConditionTaskThree/ComputerScienceConditionTaskThree/Program.cs
@@ -10,24 +10,21 @@ namespace ComputerScienceConditionTaskThree
             int inputA = int.Parse(Console.ReadLine());
             Console.Write("Введите координату b = ");
             int inputB = int.Parse(Console.ReadLine());
-
-            // Вынести в отдельные функции как сравнение, так и функцию просчета уравнения.
-            double result = EquationSolution(inputA);
             
-            CompareSolution(inputB, result);
-            // Здесь соответственно вывести на экран в зависимости от условия.
+            double result = EquationSolution(inputA);
+            bool resultCompare = CompareSolution(inputB, result);
+            
+            Console.WriteLine(resultCompare);
         }
 
-        static void CompareSolution(int inputB, double result)
+        static bool CompareSolution(int inputB, double result)
         {
-            Console.WriteLine(inputB == result);
-            // тут лучше просто сравнение inputB == result
+           return inputB == result;
         }
         
         static double EquationSolution(int x)
         {
-            // Скобки не нужны
-            return (5 * Math.Pow(x, 2) - 7 * x + 2);
+            return 5 * Math.Pow(x, 2) - 7 * x + 2;
         }
     }
 }
