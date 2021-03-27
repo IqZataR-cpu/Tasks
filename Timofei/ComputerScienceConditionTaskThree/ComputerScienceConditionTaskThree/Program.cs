cd63a4eb8a73026d34e5e5f473c8804ce9cdd6f3
yo <yo>
2021-03-27 22:34:49 +0300

    Complete task: Computer Science Condition Task Three

---
+++ Timofei/ComputerScienceConditionTaskThree/ComputerScienceConditionTaskThree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ComputerScienceConditionTaskThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату a = ");
+            int inputA = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату b = ");
+            int inputB = int.Parse(Console.ReadLine());
+
+            bool result = inputB == 5 * Math.Pow(inputA, 2) - 7 * inputA + 2;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
