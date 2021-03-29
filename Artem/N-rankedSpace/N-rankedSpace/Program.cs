f78563d971715f315e2fec08ea9bdb62c81190ab
Mikuzen <Mikuzen@github.com>
2021-03-29 11:18:04 +0300

    Distance between 2 dots on n-ranked space

---
+++ Artem/N-rankedSpace/N-rankedSpace/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace N_rankedSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность пространства");
+            int rankSpace = int.Parse(Console.ReadLine());
+
+            int[] dotA = CreateDot(rankSpace);
+            Print(dotA);
+            int[] dotB = CreateDot(rankSpace);
+            Print(dotB);
+
+            double distance = Distance(dotA, dotB);
+            Console.WriteLine($"Расстояние в {rankSpace}-пространстве равна {distance}");
+
+        }
+
+        public static int [] CreateDot(int countCoordinates)
+        {
+            Random randomNumber = new Random();
+            int[] dot = new int[countCoordinates];
+
+            for (int i = 0; i <= dot.Length - 1; i++)
+            {
+                dot[i] = randomNumber.Next(10);
+            }
+
+            return dot;
+        }
+
+        public static double Distance(int[] dotA, int[] dotB)
+        {
+            double distance = 0;
+
+            for (int i = 0; i <= dotA.Length - 1; i++)
+            {
+                distance += Math.Pow(dotA[i] - dotB[i], 2);
+            }
+
+            return Math.Sqrt(distance);
+        }
+
+        public static void Print(int[] dot)
+        {
+            
+            foreach (int i in dot)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
