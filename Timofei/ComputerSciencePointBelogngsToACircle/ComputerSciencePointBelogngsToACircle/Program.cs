6ac1a7c3787d5650192e24888477410f867c834e
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-03-30 10:11:48 +0300

    Fixme for Timofei

--- Timofei/ComputerSciencePointBelogngsToACircle/ComputerSciencePointBelogngsToACircle/Program.cs
+++ Timofei/ComputerSciencePointBelogngsToACircle/ComputerSciencePointBelogngsToACircle/Program.cs
@@ -18,8 +18,13 @@ namespace ComputerSciencePointBelogngsToACircle
             Console.WriteLine(PointBeloginsToACircleValid(x,y,radiusCircle));
         }
 
+        // Забыл область видимости указать,
+        // Нет такого слова Belogins, есть Belongs.
         static bool PointBeloginsToACircleValid(int x, int y, int radiusCircle)
         {
+            // Выражение возвращающие тру или фолс заменяются на однострочные.
+            // return radiusCircle > Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2))
+            // А если радиус = расстоянию до центра?
             if (radiusCircle > Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)))
             {
                 return true;
