93305a2bfe67d4d54d7cae837738f25c6abcee17
yo <yo>
2021-03-30 09:19:45 +0300

    Compete task:  ComputerSciencePointBelogngsToACircle

---
+++ Timofei/ComputerSciencePointBelogngsToACircle/ComputerSciencePointBelogngsToACircle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ComputerSciencePointBelogngsToACircle
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коородинаты: ");
+            Console.Write("x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            int y = int.Parse(Console.ReadLine());
+            
+            Console.Write("Введите радиус круга: ");
+            int radiusCircle = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(PointBeloginsToACircleValid(x,y,radiusCircle));
+        }
+
+        static bool PointBeloginsToACircleValid(int x, int y, int radiusCircle)
+        {
+            if (radiusCircle > Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
