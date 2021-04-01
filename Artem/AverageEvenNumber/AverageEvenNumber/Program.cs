ea57e58a43cdad903abac36e2a95eaedd51bbd40
Mikuzen <Mikuzen@github.com>
2021-04-01 23:46:14 +0300

    Average even numbers on uneven index

---
+++ Artem/AverageEvenNumber/AverageEvenNumber/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace AverageEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во элементов массива: ");
+            int countElements = int.Parse(Console.ReadLine());
+
+            int[] numbers = CreateArray(countElements);
+            Print(numbers);
+
+            double average = AverageEven(numbers);
+            Console.WriteLine("Ср. арифмитическое всех четных чисел стоящих " +
+                $"на нечетных местах равно {average}");
+        }
+
+        static int[] CreateArray(int countElements)
+        {
+            Random random = new Random();
+            int[] numbers = new int[countElements];
+
+            for(int i = 0; i <= countElements - 1; i++)
+            {
+                numbers[i] = random.Next(20);
+            }
+
+            return numbers;
+        }
+
+        static double AverageEven(int[] numbers)
+        {
+            double average = 0;
+            int countNumbers = new int();
+
+            for (int i = 0; i<= numbers.Length - 1; i += 2)
+            {
+                if (isEvenNumber(numbers[i]))
+                {
+                    average += numbers[i];
+                    countNumbers++;
+                }
+            }
+
+            return average / countNumbers;
+        }
+
+        static bool isEvenNumber(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void Print(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
