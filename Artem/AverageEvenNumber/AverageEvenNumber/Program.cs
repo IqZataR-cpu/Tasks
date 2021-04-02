67be24f5a25f96a0b4791e2776e89c2a2b420dd1
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-04-02 11:01:18 +0300

    Fixme for Artem

--- Artem/AverageEvenNumber/AverageEvenNumber/Program.cs
+++ Artem/AverageEvenNumber/AverageEvenNumber/Program.cs
@@ -4,6 +4,8 @@ namespace AverageEvenNumber
 {
     class Program
     {
+        // MSDN c# говорит нам о том, что мы должны везде указывать область видимости
+        // то есть private, public, protected по ситуации. Привыкаем!
         static void Main(string[] args)
         {
             Console.WriteLine("Введите кол-во элементов массива: ");
@@ -12,6 +14,7 @@ namespace AverageEvenNumber
             int[] numbers = CreateArray(countElements);
             Print(numbers);
 
+            // нет смысла хранить результат в переменной, можно его сразу вывести на экран
             double average = AverageEven(numbers);
             Console.WriteLine("Ср. арифмитическое всех четных чисел стоящих " +
                 $"на нечетных местах равно {average}");
@@ -22,6 +25,7 @@ namespace AverageEvenNumber
             Random random = new Random();
             int[] numbers = new int[countElements];
 
+            // пробел!!!
             for(int i = 0; i <= countElements - 1; i++)
             {
                 numbers[i] = random.Next(20);
@@ -33,8 +37,11 @@ namespace AverageEvenNumber
         static double AverageEven(int[] numbers)
         {
             double average = 0;
+            // wtf is "new int()"??? Why not "= 0"?
+            // Опять же указываем сначала объект, а потом его подчиненного,
+            // то есть numbersCount более лаконичное название. 
             int countNumbers = new int();
-
+                            // пробел!!!
             for (int i = 0; i<= numbers.Length - 1; i += 2)
             {
                 if (isEvenNumber(numbers[i]))
@@ -47,8 +54,11 @@ namespace AverageEvenNumber
             return average / countNumbers;
         }
 
+        // В английской языке предпочтительнее было бы сначала указать объект,
+        // а потом его качество, то есть isNumberEven
         static bool isEvenNumber(int number)
         {
+            // такие методы сокращаются до однострочных return number % 2 == 0;
             if (number % 2 == 0)
             {
                 return true;
