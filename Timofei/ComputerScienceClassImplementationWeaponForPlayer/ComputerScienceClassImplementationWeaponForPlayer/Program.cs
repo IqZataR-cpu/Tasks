b8cdd3b6024fdffbbc0c3e8d561e476e3f199755
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-04-06 13:43:51 +0300

    Fixme for Timofei

--- Timofei/ComputerScienceClassImplementationWeaponForPlayer/ComputerScienceClassImplementationWeaponForPlayer/Program.cs
+++ Timofei/ComputerScienceClassImplementationWeaponForPlayer/ComputerScienceClassImplementationWeaponForPlayer/Program.cs
@@ -6,10 +6,13 @@
         {
             Knife knife = new Knife("karambit", 35, 0);
             Gun pistol = new Gun("beretta", 40, 7);
-            
+            // Пробуй мыслить объектно,
+            // прокидывать примитивные параметры - это не про крутое программирование.
+            // Как пример вместо прокидывания трех параметров, ты мог прокинуть класс оружия)
+            // Вспоминаем книгу Clean Code, количество параметров в функции не должно быть больше 3... 
             Hero firstUser = new Hero("Тимофей", 100, pistol.Type, pistol.Damage, pistol.Ammunition);
             Enemy firstEnemy = new Enemy("Hui", 100, knife.Type, knife.Damage, knife.Ammunition);
-
+            
             Action.PrintPlayer(firstUser);
          
             Action.SwapWeapon(firstUser);
