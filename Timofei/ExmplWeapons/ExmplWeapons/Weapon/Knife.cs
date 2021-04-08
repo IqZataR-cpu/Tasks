07e8ff2c2a436e10b89477c0582ddd0c37a03ddb
IqZataR <Mikcha.y.12@gmail.com>
2021-04-08 15:07:02 +0300

    Example for Timofei

---
+++ Timofei/ExmplWeapons/ExmplWeapons/Weapon/Knife.cs
@@ -0,0 +1,9 @@
+﻿namespace ComputerScienceClassImplementationWeaponForPlayer
+{
+    public class Knife : SteelArms
+    {
+        public Knife(string name, int damage) : base(name, damage)
+        {
+        }
+    }
+}
---
+++ Timofei/ExmplWeapons/ExmplWeapons/Weapon/Pistol.cs
@@ -0,0 +1,9 @@
+﻿namespace ComputerScienceClassImplementationWeaponForPlayer
+{
+ public class Pistol : FireArms
+    {
+        public Pistol(string type, int damage, int ammunition) : base(type, damage, ammunition)
+        {
+        }
+    }
+}
