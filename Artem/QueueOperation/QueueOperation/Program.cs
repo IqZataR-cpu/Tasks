b277898f0d9ff57a4d6102a002ea55b691f07bb1
Mikuzen <Mikuzen@github.com>
2021-04-08 16:34:29 +0300

    Queue's operations

---
+++ Artem/QueueOperation/QueueOperation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueOperation
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            int firstElementQueue;
+            Random random = new Random();
+            int numbersCount;
+
+            Console.WriteLine("Введите количество элементов очереди: ");
+            numbersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numbersCount - 1; i++)
+            {
+                numbers.Enqueue(random.Next(100));
+            }
+
+            PrintQueue(numbers);
+
+            firstElementQueue = numbers.Dequeue();
+
+            PrintQueue(numbers);
+
+            firstElementQueue = numbers.Peek();
+
+            PrintQueue(numbers);
+        }
+
+        private static void PrintQueue(Queue<int> numbers)
+        {
+            Console.WriteLine("\nОчередь: ");
+
+            foreach (var numbersItem in numbers)
+            {
+                Console.Write($"{numbersItem} ");
+            }
+        }
+    }
+}
