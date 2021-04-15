8e74a96b819112ad1a461670bdfe2da5939bddfe
yo <yo>
2021-04-15 22:35:52 +0300

    complete task: ComputerScienceConditionYears

--- Timofei/ComputerScienceConditionYear/ComputerScienceConditionYears/Program.cs
+++ Timofei/ComputerScienceConditionYear/ComputerScienceConditionYears/Program.cs
@@ -1,32 +1,22 @@
 ﻿using System;
 
-
-// Файл и решение с тестами названо не по конвенции
 namespace ComputerScienceConditionYears
 {
-    // область видимости
     public class Program
     {
         public static void Main(string[] args)
         {
             int inputYear = int.Parse(Console.ReadLine());
-            string result = ValidLeapYear.LeepYear(inputYear);
+            string result = LeepYear(inputYear);
             
             Console.WriteLine(result);
         }
-    }
-
-    
-    // Убрать класс, сделать просто метод, 
-    // есть смысл создать класс календарь и там определять что год высокосный
-    // а так как здесь не вижу смысла в этом
-    public class ValidLeapYear
-    {
+        
         public static string LeepYear(int data)
         {
             if (data % 4 == 0)
             {
-               return "Високосный год";
+                return "Високосный год";
             }
             
             return "Обычный год";
