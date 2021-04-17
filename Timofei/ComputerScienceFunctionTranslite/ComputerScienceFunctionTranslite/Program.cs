a9cd9ed64a075f8eb8a4a4ed77e1ca3f77ff87fb
yo <yo>
2021-04-17 23:28:08 +0300

    Complete task: Computer Science Function Translite

--- Timofei/ComputerScienceFunctionTranslite/ComputerScienceFunctionTranslite/Program.cs
+++ Timofei/ComputerScienceFunctionTranslite/ComputerScienceFunctionTranslite/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 namespace ComputerScienceFunctionTranslite
 {
     public class Program
@@ -9,81 +8,103 @@ namespace ComputerScienceFunctionTranslite
         public static void Main(string[] args)
         {
             string inputUser = Console.ReadLine();
-
-            // Исправить название переменной, по сути ты обманываешь пользователей,
-            // Вот поэтому нельзя обращать слишком большое внимание на название в переменных
-            // Потому что джуны часто путаются и только вводят в замешательство.
-            // Я хочу видеть название отражающее суть, то есть translator.
-            Translator user = new Translator(inputUser);
             
-            // 
-            user.Print();
+            ITranslatorDict eng = new KeyDictionary();
+            Translator translator = new Translator(inputUser, eng);
+            
+            translator.Translate();
         }
     }
 
     public class Translator
     {
         private readonly string _input;
-        private readonly List<string> _inputString = new List<string>();
-
-        public Translator(string input)
+        private readonly List<char> _inputString = new List<char>();
+        private readonly ITranslatorDict _values;
+        
+        public Translator(string input, ITranslatorDict values)
         {
             _input = input;
+            _values = values;
         }
-
-        // Нет смысла хранить строки, если бы у нас транслировались буквенные сочетания,
-        // тогда возможно да, но это был бы и другой способ парсинга входящей строки.  
+        
         private void GetStringCharactersToArray()
         {
             for (int i = 0; i < _input.Length; i++)
             {
-                _inputString.Add(_input.ToCharArray()[i].ToString());
+                _inputString.Add(_input.ToCharArray()[i]);
             }
         }
-
-        // rename to Translate()
-        public void Print()
+        
+        public void Translate()
         {
-            // remove this method
             GetStringCharactersToArray();
             
-            // use char symbol in _input
-            foreach (var symbolString in _inputString)
+            foreach (char symbolString in _inputString)
             {
-                // use https://docs.microsoft.com/ru-ru/dotnet/api/system.collections.generic.dictionary-2.containskey?view=net-5.0#System_Collections_Generic_Dictionary_2_ContainsKey__0_
-                // remove loop from here, if(contains) KeyDictionary.TranslateString[symbol]
-                foreach (var result in KeyDictionary.TranslateString)
-                {
-                    if (symbolString == result.Key)
-                    {
-                        Console.Write(result.Value);
-                    }
+                if (_values.Values.ContainsKey(symbolString))
+                { 
+                    Console.Write(_values.Values[symbolString]); 
                 }
+                else
+                {
+                    Console.Write(symbolString);
+                }
+              
+                // if (KeyDictionary.TranslateString.TryGetValue(symbolString, out string value))
+                 // { 
+                 //     Console.Write(value); 
+                 // }
+                 // else
+                 // { 
+                 //     Console.Write(symbolString);
+                 // }
             }
         }
     }
-
-    // Make this class more flexible, dont use static.
-    // Make ITranslatorDict Interface with property Dictionary<char, string> Values { get; };
-    // Make EnglishDict implements ITranslatorDict, initialize values property with your variant of translate
-    // Create property for Translator and add dependensy with constructor param -> Translator(ITranslatorDict dict)
-    public static class KeyDictionary
+    
+    public interface ITranslatorDict
+    { 
+        Dictionary<char, string> Values { get; }
+    }
+    
+    public class KeyDictionary : ITranslatorDict
     {
-        // change string type of key to char
-        public static Dictionary<string, string> TranslateString { get; } = new Dictionary<string, string>
+        public KeyDictionary()
         {
-            {"а","a"}, {"б","b"},  {"в","v"},  {"г","g"}, {"д","d"}, {"е","e"},
-            {"ё","e"}, {"ж","eh"}, {"з","z"}, {"и","i"}, {"й","y"}, {"к","k"},
-            {"л","l"}, {"м","m"}, {"н","n"}, {"о","o"}, {"п","p"}, {"р","r"}, 
-            {"с","c"}, {"т","t"}, {"у","u"}, {"ф","f"}, {"х","h"}, {"ц","c"},
-            {"ч","ch"}, {"ш","sh"}, {"щ","h"}, {"ь",""}, {"ъ",""}, {"ы","i"},
-            {"э","e"}, {"ю","yu"}, {"я","ya"}, {" "," "}, {"А","A"},
-            {"Б","B"}, {"В","V"}, {"Г","G"}, {"Д","D"}, {"Е","E"}, {"Ё","E"},
-            {"Ж","EH"}, {"З","Z"}, {"И","I"}, {"Й","Y"}, {"К","K"}, {"Л","L"}, 
-            {"М","M"}, {"Н","N"}, {"О","O"}, {"П","P"}, {"Р","R"}, {"С","C"}, 
-            {"Т","T"}, {"У","U"}, {"Ф","F"}, {"Х","H"}, {"Ц","C"}, {"Ч","CH"},
-            {"Ш","SH"}, {"Щ","H"}, {"Ь",""}, {"Ъ",""}, {"Ы","I"}, {"Э","E"},
-            {"Ю","YU"}, {"Я","YA"},
-        };
+            this.Values = new Dictionary<char, string>
+            {
+                {'а', "a"}, {'б', "b"}, {'в', "v"}, {'г', "g"}, {'д', "d"}, {'е', "e"},
+                {'ё', "e"}, {'ж', "h"}, {'з', "z"}, {'и', "i"}, {'й', "y"}, {'к', "k"},
+                {'л', "l"}, {'м', "m"}, {'н', "n"}, {'о', "o"}, {'п', "p"}, {'р', "r"},
+                {'с', "c"}, {'т', "t"}, {'у', "u"}, {'ф', "f"}, {'х', "h"}, {'ц', "c"},
+                {'ч', "ch"}, {'ш', "sh"}, {'щ', "h"}, {'ь', ""}, {'ъ', ""}, {'ы', "i"},
+                {'э', "e"}, {'ю', "yu"}, {'я', "ya"}, {' ', " "}, {'А', "A"},
+                {'Б', "B"}, {'В', "V"}, {'Г', "G"}, {'Д', "D"}, {'Е', "E"}, {'Ё', "E"},
+                {'Ж', "H"}, {'З', "Z"}, {'И', "I"}, {'Й', "Y"}, {'К', "K"}, {'Л', "L"},
+                {'М', "M"}, {'Н', "N"}, {'О', "O"}, {'П', "P"}, {'Р', "R"}, {'С', "C"},
+                {'Т', "T"}, {'У', "U"}, {'Ф', "F"}, {'Х', "H"}, {'Ц', "C"}, {'Ч', "CH"},
+                {'Ш', "SH"}, {'Щ', "H"}, {'Ь', ""}, {'Ъ', ""}, {'Ы', "I"}, {'Э', "E"},
+                {'Ю', "YU"}, {'Я', "YA"},
+            };
+        }
+        // change string type of key to char
+        // public Dictionary<char, string> TranslateString { get; } = new Dictionary<char, string>
+        // {
+        //     {'а',"a"}, {'б',"b"},  {'в',"v"},  {'г',"g"}, {'д',"d"}, {'е',"e"},
+        //     {'ё',"e"}, {'ж',"h"}, {'з',"z"}, {'и',"i"}, {'й',"y"}, {'к',"k"},
+        //     {'л',"l"}, {'м',"m"}, {'н',"n"}, {'о',"o"}, {'п',"p"}, {'р',"r"}, 
+        //     {'с',"c"}, {'т',"t"}, {'у',"u"}, {'ф',"f"}, {'х',"h"}, {'ц',"c"},
+        //     {'ч',"ch"}, {'ш',"sh"}, {'щ',"h"}, {'ь',""}, {'ъ',""}, {'ы',"i"},
+        //     {'э',"e"}, {'ю',"yu"}, {'я',"ya"}, {' '," "}, {'А',"A"},
+        //     {'Б',"B"}, {'В',"V"}, {'Г',"G"}, {'Д',"D"}, {'Е',"E"}, {'Ё',"E"},
+        //     {'Ж',"H"}, {'З',"Z"}, {'И',"I"}, {'Й',"Y"}, {'К',"K"}, {'Л',"L"}, 
+        //     {'М',"M"}, {'Н',"N"}, {'О',"O"}, {'П',"P"}, {'Р',"R"}, {'С',"C"}, 
+        //     {'Т',"T"}, {'У',"U"}, {'Ф',"F"}, {'Х',"H"}, {'Ц',"C"}, {'Ч',"CH"},
+        //     {'Ш',"SH"}, {'Щ',"H"}, {'Ь',""}, {'Ъ',""}, {'Ы',"I"}, {'Э',"E"},
+        //     {'Ю',"YU"}, {'Я',"YA"},
+        // };
+
+        public Dictionary<char, string> Values { get; }
     }
 }
