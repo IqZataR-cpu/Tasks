a34df5c55d757e98bda36d66abff66879f560676
Mikuzen <Mikuzen@github.com>
2021-04-19 11:46:32 +0300

    Definition quarter hour

---
+++ Artem/QuarterHour/QuarterHour/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace QuarterHour
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int min;
+            string quarter = null;
+
+            Console.WriteLine($"Сейчас {min = DefinitionMinute()} минут, {quarter = QuarterHour(min)} четверть часа");
+        }
+
+        private static int DefinitionMinute()
+        {
+            int minute;
+            Random random = new Random();
+
+            minute = random.Next(0, 60);
+
+            return minute;
+        }
+
+        private static string QuarterHour(int minute)
+        {
+            string quarter = null;
+
+            if (minute <= 15)
+            {
+                return quarter = "первая";
+            }
+            else if (minute > 15 && minute <= 30)
+            {
+                return quarter = "вторая";
+            }
+            else if (minute > 30 && minute <= 45)
+            {
+                return quarter = "третья";
+            }
+            else if (minute > 45 && minute <= 59)
+            {
+                return quarter = "четвертая";
+            }
+
+            return quarter;
+        }
+    }
+}
