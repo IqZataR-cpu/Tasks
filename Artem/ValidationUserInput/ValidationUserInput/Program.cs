bef20b6760a01bdb76a5f0aad8f234530741630c
Mikuzen <Mikuzen@github.com>
2021-04-19 11:30:06 +0300

    Validation user input

---
+++ Artem/ValidationUserInput/ValidationUserInput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ValidationUserInput
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int userInput;
+
+            Console.WriteLine("Введите любое число: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (isNumberOfTen(userInput))
+            {
+                Console.WriteLine("Верно");
+            }
+            else
+            {
+                Console.WriteLine("Неверно");
+            }
+        }
+
+        private static bool isNumberOfTen(int number)
+        {
+            return number == 10;
+        }
+    }
+}
