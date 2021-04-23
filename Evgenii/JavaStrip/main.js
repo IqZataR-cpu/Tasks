dcd70664cf9d5d5f9b7efc461b7875524a1bde1d
Kerollinenot <zhenya.petrovskiy.2015@mail.ru>
2021-04-23 17:15:15 +0300

    Create JavaStrip

---
+++ Evgenii/JavaStrip/main.js
@@ -0,0 +1,12 @@
+function changeText(){
+    let centerText = document.getElementById('centerText');
+    if (centerText.textContent == 'Hello World')
+    {
+        centerText.textContent = 'JavaStrip';
+    }
+    else
+    {
+        centerText.textContent = 'Hello World';
+    }
+
+}
