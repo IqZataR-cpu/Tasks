63a31986d3801fb7abfa2530d22127ad01b1a2ff
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-04-23 17:34:10 +0300

    Fixme for Evgenii

--- Evgenii/JavaStrip/main.js
+++ Evgenii/JavaStrip/main.js
@@ -1,12 +1,20 @@
-function changeText(){
+function changeText()/*тут нужен пробел!*/{
     let centerText = document.getElementById('centerText');
+    // Тут бы стоило поставить enter.
     if (centerText.textContent == 'Hello World')
     {
         centerText.textContent = 'JavaStrip';
     }
+    // пишем return; стараемся не использовать else
     else
     {
         centerText.textContent = 'Hello World';
     }
+    // Лишний Enter
 
 }
+/*
+ В задании сказано найти кнопку по идентификатору(id, можно и любой другой)
+ и навесить на него функцию изменяющую текст или сразу просто изменить на старте, как тебе хочется.
+ Важно продемонстрировать навыки поиска элементов на странице по селектору.
+*/
