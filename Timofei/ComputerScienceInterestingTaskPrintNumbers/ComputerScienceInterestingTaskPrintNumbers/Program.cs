9a615d735407170ce4eb39e90b1191cbb0677a73
yo <yo>
2021-04-25 13:02:48 +0300

    Complete task: ComputerScienceInterestingTaskPrintNumbers

---
+++ Timofei/ComputerScienceInterestingTaskPrintNumbers/ComputerScienceInterestingTaskPrintNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace ComputerScienceInterestingTaskPrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            
+            Print(i);
+        }
+        
+        static int Print(int i)
+        {
+            if (i < 100)
+            {
+                i++;
+                
+                Console.Write($"{i}, ");
+                
+                Print(i);
+            }
+
+            return i;
+        }
+    }
+}
