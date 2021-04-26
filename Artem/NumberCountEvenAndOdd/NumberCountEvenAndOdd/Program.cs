1889ebd73371a1ed386b688c6114e6a3c94809f1
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-04-26 12:26:06 +0300

    Fixme for Artem

--- Artem/NumberCountEvenAndOdd/NumberCountEvenAndOdd/Program.cs
+++ Artem/NumberCountEvenAndOdd/NumberCountEvenAndOdd/Program.cs
@@ -13,9 +13,11 @@ namespace NumberCountEvenAndOdd
             Console.WriteLine("Введите число");
             input = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"В числе {input}, четных чисел {evenCount = CountEven(input)}, " +
+            // Я сделал пример как круче форматировать. Исправлять уже ничего не нужно.
+            Console.WriteLine(
+                $"В числе {input}, четных чисел {evenCount = CountEven(input)}, " +
                 $"нечетных чисел {oddCount = CountOdd(input)}"
-                );
+            ); // ошибка табуляции, ПИТОН НЕ ПРОЩАЕТ!!! Здесь нужен был минус таб.
         }
 
         private static int CountEven(int number)
@@ -24,6 +26,9 @@ namespace NumberCountEvenAndOdd
             
             while (number > 0)
             {
+                // не совсем понимаю зачем рекурсия, если сверху цикл,
+                // если бы там было просто условие, тогда ок.
+                // обычно рекурсия заменяет цикл, но в целом идею понял, прикольно.
                 count = CountEven(number / 10);
 
                 if (number % 2 == 0)
@@ -44,6 +49,9 @@ namespace NumberCountEvenAndOdd
 
             while (number > 0)
             {
+                // не совсем понимаю зачем рекурсия, если сверху цикл,
+                // если бы там было просто условие, тогда ок.
+                // обычно рекурсия заменяет цикл, но в целом идею понял, прикольно.
                 count = CountOdd(number / 10);
 
                 if (number % 2 == 1)
