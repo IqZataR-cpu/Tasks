ea1b4e9b2f27704755b1df99a8586ca98b096fd6
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-04-26 12:38:10 +0300

    Fixme for Timofei

--- Timofei/HhmlCssJsTask/HtmlCsJsLearningTaskOutputHelloWorld/main.js
+++ Timofei/HhmlCssJsTask/HtmlCsJsLearningTaskOutputHelloWorld/main.js
@@ -1,5 +1,25 @@
 window.onclick = ReplaceText;
 
+
+/* 
+Для справки:
+1. Все гуд, но в лучше на window ничего не навешивать.
+2. Если ты сделаешь локальную переменную
+let mainContainer = document.getElementById('main');
+То сохранишь ссылку на элемент с этим id.
+и в функции сможешь сделать просто mainContainer.innerHtml = 'JavaStrip';
+тем самым ты не будешь заново искать элемент по id при каждом клике.
+
+Полный код был бы такой:
+let mainContainer = document.getElementById('main');
+
+mainContainer.onclick = ReplaceText;
+
+function ReplaceText(){
+    mainContainer.innerHTML = 'JavaStrip';
+}
+
+*/
 function ReplaceText(){
     document.getElementById('main').innerHTML = 'JavaStrip';
 }
