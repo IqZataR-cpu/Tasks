85c17de4cad5b7d3a39cf7b3c32f4bb8063fba53
IqZataR-cpu <54170810+IqZataR-cpu@users.noreply.github.com>
2021-04-26 18:56:19 +0300

    Update main.js

--- Evgenii/JavaStrip/main.js
+++ Evgenii/JavaStrip/main.js
@@ -8,6 +8,24 @@ function changeText()/*тут нужен пробел!*/{
     }
         centerText.textContent = 'Hello World';
 }
+
+
+let btn = document.querySelector('.mainButton');
+let centerText = document.getElementById('centerText');
+
+btn.onclick = changeText;
+
+function changeText(event) {
+    if (centerText.textContent == 'Hello World')
+    {
+        centerText.textContent = 'JavaStrip';
+        return;
+    }
+    
+    centerText.textContent = 'Hello World';
+}
+
+
 /*
  В задании сказано найти кнопку по идентификатору(id, можно и любой другой)
  и навесить на него функцию изменяющую текст или сразу просто изменить на старте, как тебе хочется.
