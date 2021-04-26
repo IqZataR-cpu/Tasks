376509f6a64fac8b2a85e119dbd007dcdb2baf66
Mikuzen <Mikuzen@github.com>
2021-04-26 11:08:07 +0300

    Invert number

---
+++ Artem/InvertNumber/InvertNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InvertNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int input;
+            string invertNumber;
+
+            Console.WriteLine("Введите число: ");
+            input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Инвертированное число {invertNumber = Inversion(input)}");
+        }
+
+        private static string Inversion(int number)
+        {
+            string invertNumber = number.ToString();
+            Char[] digits = invertNumber.ToCharArray();
+
+            Array.Reverse(digits);
+
+            return invertNumber = new string(digits);
+        }
+    }
+}
