57c4c3bac064b377c9675f6c5a6cf8640323e4c6
Mikuzen <Mikuzen@github.com>
2021-04-26 10:31:02 +0300

    Print one to hundred

---
+++ Artem/PrintOneToHundred/PrintOneToHundred/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintOneToHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = 'A' / 'A';
+            String s = "..........";
+
+            for (int i = one; i <= s.Length * s.Length; i++)
+            {
+                Console.Write($"{i} ");
+            }                
+        }
+    }
+}
