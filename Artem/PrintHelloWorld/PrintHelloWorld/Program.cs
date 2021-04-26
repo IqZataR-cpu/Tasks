6ae4673b838e42102979eb43d84410b773a5d0a9
Mikuzen <Mikuzen@github.com>
2021-04-26 10:56:15 +0300

    Print hello world

---
+++ Artem/PrintHelloWorld/PrintHelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           if (Console.WriteLine("Hello world") is string)
+            {
+
+            }
+        }
+    }
+}
