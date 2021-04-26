8066e3a7a0e42608f000245c707db630eee1d5f3
Mikuzen <Mikuzen@github.com>
2021-04-26 10:36:18 +0300

    Print one to hundred

--- Artem/PrintOneToHundred/PrintOneToHundred/Program.cs
+++ Artem/PrintOneToHundred/PrintOneToHundred/Program.cs
@@ -2,9 +2,9 @@
 
 namespace PrintOneToHundred
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int one = 'A' / 'A';
             String s = "..........";
