8ec1a95d65c53a3b7c87cce291d7c5af758248b7
yo <yo>
2021-04-29 08:02:23 +0300

    Program by Storage personal data

--- Timofei/ComputerScienceProgramPresonalDataStorage/ComputerScienceProgramPresonalDataStorage/Program.cs
+++ Timofei/ComputerScienceProgramPresonalDataStorage/ComputerScienceProgramPresonalDataStorage/Program.cs
@@ -1,158 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Data.OleDb;
-using System.IO;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
+﻿ using System;
+ using System.Collections.Generic;
+ using System.IO;
+ using System.Linq;
+ using System.Security.Cryptography;
+ using TestEncryptedDecriptedTextInFile;
 
-namespace ComputerScienceProgramPresonalDataStorage
+ namespace TestEncryptedDecriptedTextInFile
 {
-    public class Program
+   public enum ChoiceSearch
     {
-        public static void Main(string[] args)
-        {
-            DataStorage data = new DataStorage();
-           
-            data.FormGreeting(); 
-        }
+        search,
+        all
     }
-
-    public enum Search
+    class Program
     {
-        all,
-        search
-    }
-    
-    public class DataBase
-    {
-        private static readonly string _path = @"C:\Users\User\Desktop\db";
-        private static readonly string _name = @"\data.txt";
-        private DirectoryInfo _dbInfo = new DirectoryInfo(_path);
-
-        public void ExistsDataBase()
+        static void Main(string[] args)
         {
-            if (!_dbInfo.Exists)
-            {
-                _dbInfo.Create();
-            }
-        }
+            Encrypted encrypted = new Encrypted();
+            Decrypted decrypted = new Decrypted();
+            Key key = new Key();
 
-        public void SelectFromDB(Search search)
-        {
-            string temp = "";
-            Char Sep = ',';
+            key.AddKey();
 
-            using (FileStream file = File.OpenRead(_path + _name))
-            {
-                byte[] array = new byte[file.Length];
-
-                char[] chrArray = new char[file.Length];
-
-                List<string> strArray = new List<string>();
-
-                file.Read(array, 0, array.Length);
-
-                chrArray = System.Text.Encoding.Default.GetChars(array);
-
-                foreach (var p in chrArray)
-                {
-                    if (p != Sep)
-                    {
-                        temp += p;
-                    }
-                    else
-                    {
-                        strArray.Add(temp);
-
-                        temp = "";
-                    }
-                }
-
-                if (search == Search.all)
-                {
-                    ViewAll(strArray);
-                }
-                else
-                {
-                    ViewSearch(strArray);
-                }
-            }
-        }
-
-        public void ViewAll(List<string> strArray)
-        {
-            foreach (var str in strArray)
-            {
-                Console.Write(str);
-            }
-        }
-        
-        public void ViewSearch(List<string> strArray)
-        {
-            Console.Write("Введите наименование: ");
-            string tittle = Console.ReadLine();
-
-            for (int i = 0; i < strArray.Count; i += 3)
-            {
-                if (strArray[i] == "\n" + tittle)
-                {
-                    Console.Write(strArray[i]);
-                    Console.Write(strArray[i + 1]);
-                    Console.WriteLine(strArray[i + 2]);
-
-                    return;
-                }
-            }
-        }
-
-        public void InsertFromDB()
-        {
-            Console.Write("Введите наименование: ");
-            string tittle = Console.ReadLine(); // название сохраняемого значения
-            Console.Write("\nВведите пароль: ");
-            string password = Console.ReadLine(); // пароль
-            Console.Write("\nВведите наименование: ");
-            string description = Console.ReadLine(); // описание
-
-            using (FileStream file = new FileStream(_path + _name, FileMode.Append))
-            {
-                byte[] tittleArray = System.Text.Encoding.Default.GetBytes("\n" + tittle + ",");
-                byte[] passwordArray = System.Text.Encoding.Default.GetBytes("\n" + password + ",");
-                byte[] descriptionArray = System.Text.Encoding.Default.GetBytes("\n" + description + ",");
-               
-                file.Write(tittleArray, 0, tittleArray.Length);
-                file.Write(passwordArray, 0, passwordArray.Length);
-                file.Write(descriptionArray, 0, descriptionArray.Length);
-            }
-        }
-    }
-    
-    public class DataStorage
-    {
-        public void FormGreeting()
-        {
-            DataBase db = new DataBase();
-            
-            db.ExistsDataBase(); // Создаем папку, где должен располагаться файл
-            
             while (true)
             {
                 ShowOptions();
-                
                 int choice = int.Parse(Console.ReadLine());
-
+                
                 switch (choice)
                 {
                     case 1:
-                        db.SelectFromDB(Search.search);
+                        decrypted.DecryptedText(ChoiceSearch.search);
                         break;
                     case 2:
-                        db.InsertFromDB();
+                        Console.Write("Введите наименование: ");
+                        string tittle = Console.ReadLine(); 
+                        Console.Write("\nВведите пароль: ");
+                        string password = Console.ReadLine();
+                        Console.Write("\nВведите наименование: ");
+                        string description = Console.ReadLine();
+                    
+                        encrypted.EncryptedText(tittle, password, description);
                         break;
                     case 3:
-                        db.SelectFromDB(Search.all);
+                        decrypted.DecryptedText(ChoiceSearch.all);
                         break; 
                     case 4:
                         return;
@@ -163,7 +54,7 @@ namespace ComputerScienceProgramPresonalDataStorage
                 }
             }
         }
-
+        
         private static void ShowOptions()
         {
             Console.WriteLine("1. Поиск.");
@@ -176,8 +67,216 @@ namespace ComputerScienceProgramPresonalDataStorage
         {
             Console.Clear();
             
-            Console.WriteLine("Нет такого варианта.\n" +
-                              "Попробуй ещё раз.");
+            Console.WriteLine("Нет такого варианта.\n Попробуй ещё раз.");
         }
     }
 }
+ public class Key : Path
+ {
+     public void AddKey() 
+     {
+         try
+         {
+             if (!File.Exists(_pathSettings + _nameSettings))
+             {
+                 using (FileStream file = new FileStream(_pathSettings + _nameSettings, FileMode.OpenOrCreate))
+                 {
+                     Console.WriteLine("Введите ключ (16 cимволов): ");
+            
+                     string inputKey =  Console.ReadLine();
+                     char[] ch = inputKey.ToCharArray();
+                         
+                     byte[] key = System.Text.Encoding.UTF8.GetBytes(ch);
+                
+                     file.Write(key, 0, key.Length);
+                 }
+             }
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine($"Ошибка. {e}");
+             throw;
+         }
+     }
+
+     public static byte[] GetKey()
+     {
+         try
+         {
+             using (FileStream file = File.OpenRead(_pathSettings + _nameSettings))
+             {
+                 byte[] array = new byte[file.Length];
+              
+                 file.Read(array, 0, array.Length);
+                 
+                 return array;
+             }
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine($"Ошибка. {e}");
+             throw;
+         }
+     }
+ }
+ 
+ public abstract class Path
+ {
+     protected static string _path = @"C:\Users\User\Desktop\db\";
+     protected static string _name = @"data.txt";
+     
+     protected static string _pathSettings = @"C:\Users\User\Desktop\db\";
+     protected static string _nameSettings = @"settings.txt";
+ }
+
+ 
+ public class Encrypted : Path
+ {
+     public void EncryptedText(string tittle, string password, string description)
+     {
+         try
+         {
+             using (FileStream fileStream = new FileStream(_path + _name, FileMode.Append))
+             {
+                 using (Aes aes = Aes.Create())
+                 {
+                     byte[] key = Key.GetKey();
+                     aes.Key = key;
+
+                     byte[] iv = aes.IV;
+                     
+                     fileStream.Write(iv, 0, iv.Length);
+
+                     using (CryptoStream cryptoStream = new CryptoStream(fileStream, aes.CreateEncryptor(),CryptoStreamMode.Write))
+                     {
+                         using (StreamWriter encryptWriter = new StreamWriter(cryptoStream))
+                         {
+                             encryptWriter.Write($"{"," + tittle + ","}");
+                             encryptWriter.Write($"{"," + password + ","}");
+                             encryptWriter.Write($"{"," + description + ","}");
+                         }
+                     }
+                 }
+             }
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine($"Ошибка. {e}");
+             throw;
+         }
+     }
+ }
+ 
+ public class Decrypted : Path
+ {
+     public void DecryptedText(ChoiceSearch choiceSearch)
+     {
+         try
+         {
+             using (FileStream fileStream = new FileStream(_path + _name, FileMode.Open))
+             {
+                 using (Aes aes = Aes.Create())
+                 {
+                     byte[] iv = new byte[aes.IV.Length];
+                     int numBytesToRead = aes.IV.Length;
+                     int numBytesRead = 0;
+                     while (numBytesToRead > 0)
+                     {
+                         int n = fileStream.Read(iv, numBytesRead, numBytesToRead);
+                         if (n == 0) break;
+
+                         numBytesRead += n;
+                         numBytesToRead -= n;
+                     }
+
+                     byte[] key = Key.GetKey();
+
+                     using (CryptoStream cryptoStream = new CryptoStream(
+                         fileStream,
+                         aes.CreateDecryptor(key, iv),
+                         CryptoStreamMode.Read))
+                     {
+                         using (StreamReader decryptReader = new StreamReader(cryptoStream))
+                         {
+                             string decryptedMessage = decryptReader.ReadToEnd();
+                             
+                             string temp = "";
+                             Char[] separator = new char[]
+                             {
+                                 ',',
+                                 '\r',
+                                 '\n',
+                                 '\0',
+                                 '/',
+                             };
+         
+                             char[] chrArray = new char[decryptedMessage.Length];
+                             List<string> strArray = new List<string>();
+         
+                             chrArray = decryptedMessage.ToCharArray();
+                          
+                             foreach (var p in chrArray)
+                             {
+                                 if (!separator.Contains(p))
+                                 {
+                                     temp += p;
+                                 }
+                                 else 
+                                 {
+                                     strArray.Add(temp);
+         
+                                     temp = "";
+                                 }
+                             }
+
+                             if (choiceSearch == ChoiceSearch.all)
+                             {
+                                 SearchAll(strArray);
+                             }
+                             else if (choiceSearch == ChoiceSearch.search)
+                             {
+                                 SearchByName(strArray);
+                             }
+                         }
+                     }
+                 }
+             }
+         }
+         catch (Exception ex)
+         {
+             Console.WriteLine($"The decryption failed. {ex}");
+         }
+         
+         Console.WriteLine("Нажми на кнопку чтобы продолжить");
+         Console.ReadLine();
+     }
+
+     private void SearchAll(List<string> strArray)
+     {
+         foreach (var str in strArray)
+         {
+             if (str != "")
+             {
+                 Console.WriteLine($"{str}");
+             }
+         }
+     }
+     
+     private void SearchByName(List<string> strArray)
+     {
+         Console.Write("Введите наименование: ");
+         string tittle = Console.ReadLine();
+         
+         for (int i = 1; i < strArray.Count; i += 2)
+         {
+             if (strArray[i] == tittle)
+             {
+                 Console.WriteLine(strArray[i]);
+                 Console.WriteLine(strArray[i + 2]);
+                 Console.WriteLine(strArray[i + 4]);
+                 
+                 return;
+             }
+         }
+     }
+ }
